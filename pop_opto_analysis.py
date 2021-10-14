b2b780b8951958369f675edf936831434638cb38
Jake Wright <wrightwj8@gmail.com>
2021-10-14 13:01:01 -0700

    Adding load_mat_files.py, mat_load_testing.ipynb, pop_opto_analysis.py, utilities.py files

---
+++ pop_opto_analysis.py
@@ -0,0 +1,252 @@
+import numpy as np
+import pandas as pd
+import scipy as sy
+from scipy import stats
+from itertools import compress
+import random
+import utilities as util
+
+
+class population_opto_analysis():
+    '''Class to determine if neurons are being significantly activated
+        by optogenetic stimulation.'''
+
+    def __init__(self, imaging_data, behavior_data, sampling_rate=30,
+                 window = [-2,2], stim_len=1, to_plot=False):
+        ''' __init__- Initilize population_opto_analysis Class.
+
+            CREATOR
+                William (Jake) Wright 10/7/2021
+
+            INPUT PARAMETERS
+                imaging_data - dictionary of the calcium imaging data outpt from
+                               the load_mat functions. Also accepts a list of
+                               dictionaries if you wish to analyze more than one
+                               session. More than one session is required for
+                               power_curve method.
+
+                behavior_data - dictionary of the behavior data output from the
+                                load_mat functions. Also accepts a list of
+                                dictionaries if you wish to analyze more than one.
+
+                sampling_rate - scaler specifying the image sampling rate. Default
+                                is set to 30hz.
+
+                window - list specifying the time before and after the opto stim
+                         you want to visualize and assess (e.g. [-2,2] for two sec
+                         before and after). Default is set to [-2,2].
+
+                stim_len - scaler specifying how long opto stim was delivered for.
+                           Default is set to 1 sec
+
+                to_plot - boolean True or False of weather or not you wish to
+                          plot all figures generated.
+
+
+                '''
+
+        # Storing the initial input data
+        self.imaging = imaging_data
+        self.behavior = behavior_data
+        self.sampling_rate = sampling_rate
+        self.window = window
+        self.to_plot = to_plot
+        self.before_t = window[0] # before window in time(s)
+        self.before_f = window[0]*sampling_rate # before window in frames
+        self.after_t = window[1]
+        self.after_f = window[1]*sampling_rate
+        self.stim_len = stim_len
+        self.stim_len_f = stim_len*sampling_rate
+        
+        # Pulling data from the inputs that will be used
+        ROIs = []
+        for i in list(imaging_data['ROIs'][:-1]):
+            ROIs.append('ROI ' + str(i))
+        dFoF = pd.DataFrame(data=imaging_data['processed_dFoF'].T,columns=ROIs)
+
+        self.ROIs = ROIs
+        self.dFoF = dFoF
+
+        # Select on the trials that were imaged
+        i_trials = behavior_data['imaged_trials']
+        i_trials = i_trials == 1
+        behavior = list(compress(behavior_data['behavior_frames'],i_trials))
+        itis = []
+        for i in behavior:
+            itis.append(i['states']['iti2'])
+
+        # Making sure the iti intervals are consistent and within imaging period
+        for i in range(len(itis)):
+            if itis[i][1] > len(dFoF):
+                itis = itis[0:i-1]
+            else:
+                pass
+        for i in range(len(itis)):
+            if (itis[i][1] - itis[i][0]) - self.stim_len_f == 1:
+                itis[i] = [itis[i][0],itis[i][1]-1]
+            elif (itis[i][1] - itis[i][0]) - self.stim_len_f == -1:
+                itis[i] = [itis[i][0],itis[i][1]+1]
+            elif (itis[i][1] - itis[i][0]) == self.stim_len_f:
+                itis[i] = itis[i]
+            else:
+                del itis[i]
+        
+
+        self.i_trials = i_trials
+        self.behavior = behavior
+        self.itis = itis
+        self.method = None
+        self.sig_results = None
+
+
+    def opto_before_after_means(self, data=None,single=None):
+        if data is None:
+            data = self.dFoF
+        else:
+            data = data
+        if single is None:
+            single = False
+        else:
+            single = single
+        all_befores, all_afters = util.get_before_after_means(activity=data,
+                                                              timestamps=self.itis,
+                                                              window=self.window,
+                                                              sampling_rate=self.sampling_rate,
+                                                              offset=False,single=single)
+
+        return all_befores, all_afters
+
+    def opto_trace_mean_sems(self,data=None):
+        if data is None:
+            data = self.dFoF
+        else:
+            data = data
+  
+        new_timestamps = []
+        for i in self.itis:
+            new_timestamps.append(i[0])
+        new_window = [self.window[0],self.window[1]+self.stim_len]
+        
+        roi_stim_epochs,roi_mean_sems = util.get_trace_mean_sem(activity=data,
+                                                                timestamps=new_timestamps,
+                                                                window=new_window,
+                                                                sampling_rate=self.sampling_rate)
+
+        return roi_stim_epochs, roi_mean_sems
+
+
+    def significance_testing(self, method):
+        ''' Method to determine if each ROI was significantly activated by
+            optogenetic stimulation.
+
+            INPUT PARAMETERS
+                method - string specifying which method is to be used to test
+                         significance. Currently coded to accept:
+                         
+                             'test' - Performs Wilcoxon Signed-Rank Test
+                             'shuff' - compares the real difference in activity
+                                       against a shuffled distribution '''
+        self.method = method
+
+        before_m, after_m = self.opto_before_after_means()
+
+        if method == 'test':
+            pValues = []
+            rankValues = []
+            diffs = []
+            for before, after in zip(before_m,after_m):
+                b = before
+                a = after
+
+                # Perform Wilcoxon signed-rank test; significance set at 0.01
+                rank, pVal = stats.wilcoxon(b,a)
+                pValues.append(pVal)
+                rankValues.append(rank)
+                diffs.append(np.mean(np.array(a)-np.array(b)))
+                
+            pValue_dict = dict(zip(self.dFoF.columns,pValues))
+            rank_dict = dict(zip(self.dFoF.columns,rankValues))
+            diff_dict = dict(zip(self.dFoF.columns,diffs))
+
+            
+            # Assess significance and differences
+            sig = []
+            for key,value in pValue_dict.items():
+                if value < 0.01:
+                    sig.append(1)
+                else:
+                    sig.append(0)
+            sig_dict = dict(zip(self.dFoF.columns,sig))
+            
+            #Put final results in dictionary
+            sig_results = {}
+            for key,value in pValue_dict.items():
+                sig_results[key] = {'pvalue':value,
+                                   'rank':rank_dict[key],
+                                   'diff':diff_dict[key],
+                                   'sig':sig_dict[key]}
+            self.sig_results = sig_results
+            
+        elif method == 'shuff':
+            data = self.dFoF.copy()
+            real_diffs = [] 
+            shuff_diffs = [] 
+            bounds = [] 
+            sigs = []
+            smallest = self.sampling_rate # smallest shift if 1 second
+            biggest = 300 * self.sampling_rate # biggest shift is 5 min
+            
+            # Assess each ROI individually
+            for col in data.columns:
+                d = self.dFoF[col]
+                
+                # Get the real difference
+                before, after = self.opto_before_after_means(data=d,single=True)
+                r_diff = np.mean(np.array(after)-np.array(before))
+                # Perform 1000 shuffles
+                s_diffs = []
+                for i in range(1000):
+                    n = random.randint(smallest,biggest)
+                    s_d = np.copy(d)
+                    shuff_d = np.roll(s_d,n)
+                    
+                    shuff_before, shuff_after = self.opto_before_after_means(data=shuff_d,
+                                                                             single=True)
+                    s_diffs.append(np.mean(np.array(shuff_after)-np.array(shuff_before)))
+                
+                # Assess significance
+                upper = np.percentile(s_diffs, 95)
+                lower = np.percentile(s_diffs, 5)
+                
+                if lower <= r_diff <= upper:
+                    sig = 0
+                else:
+                    sig = 1
+                
+                # Append values for each ROI
+                real_diffs.append(r_diff)
+                shuff_diffs.append(s_diffs)
+                bounds.append((upper,lower))
+                sigs.append(sig)
+            
+            # Put final results in dictionary
+            sig_results = {}
+            for col, real, shuff, b, sig in zip(data.columns,real_diffs,
+                                                      shuff_diffs,bounds, sigs):
+                sig_results[col] = {'real_diff':real,'shuff_diff':shuff,
+                                    'bounds':b, 'sig':sig}
+            self.sig_results = sig_results
+        
+        return sig_results
+    
+    
+    def power_curve():
+        ## Need to change the code above and this code so that it can accept
+        ## lists of dataframes for the different sessions
+                    
+            
+            
+                
+                
+                
+                    
---
+++ utilities.py
@@ -0,0 +1,150 @@
+import numpy as np
+import pandas as pd
+import scipy as sy
+from scipy import stats
+
+
+def get_before_after_means(activity, timestamps, window,
+                           sampling_rate=30, offset=False, single=False):
+
+    ''' Function to get the mean activity before and after a specific
+        behavioral event.
+
+        CREATOR
+            William (Jake) Wright 10/11/2021
+
+        USAGE
+            all_befores, all_afters = get_before_after_mean(activity,timestamps,window,sampling_rate)
+
+        INPUT PARAMETERS
+            activity - dataframe of neural activity, with each column
+                       corresponding to each ROI. Can also be a single column vector
+                       for a single ROI
+
+            timestamps - a list of timestamps corresponding to the imaging
+                         frame where each behavioral event occured
+
+            window - list specifying the time before and after the behavioral
+                     event you want to assess (e.g. [-2,2] for 2 secs before
+                     and after.
+
+            sampling_rate - scaler specifying the image sampling rate. Default
+                            is set to 30hz.
+
+            offset - boolean term indicating if the timestamps include the offset of behavioral
+                     event and if this should be used to determine the after period. Default is
+                     set to false.
+            
+            single - boolean term indicating if the input data is a single ROI or a dataframe. 
+                     Default is set to False to read a dataframe.
+
+        OUTPUT PARAMETERS
+            all_befores - a list containing all the before means for each ROI
+
+            all_afters - a list constining all the after means for each ROI '''
+
+    if offset == False:
+        if len(timestamps[0]) == 1:
+            stamp1 = timestamps
+            stamp2 = timestamps
+        elif len(timestamps[0]) == 2:
+            stamps = []
+            for i in timestamps:
+                stamps.append(i[0])
+            stamp1 = stamps
+            stamp2 = stamps
+        else:
+            return print('Too many indicies in each timestamps !!!')
+    else:
+        stamp1 = []
+        stamp2 = []
+        for i in timestamps:
+            stamp1.append(i[0])
+            stamp2.append(i[1])
+    before_f = window[0]*sampling_rate
+    after_f = window[1]*sampling_rate
+
+    all_befores = []
+    all_afters = []
+    if single is False:
+        for j in activity.columns:
+            d = activity[j]
+            before_values = []
+            after_values = []
+            for s1, s2 in zip(stamp1,stamp2):
+                before_values.append(np.mean(d[s1+before_f:s1]))
+                after_values.append(np.mean(d[s2:s2+after_f]))
+            all_befores.append(before_values)
+            all_afters.append(after_values)
+            
+    else:
+        d = activity
+        before_values = []
+        after_values = []
+        for s1, s2 in zip(stamp1,stamp2):
+            before_values.append(np.mean(d[s1+before_f:s1]))
+            after_values.append(np.mean(d[s2:s2+after_f]))
+        all_befores.append(before_values)
+        all_afters.append(after_values)
+
+    return all_befores, all_afters
+
+
+def get_trace_mean_sem(activity,timestamps,window,sampling_rate=30):
+    ''' Function to get the mean and sem of neural activity around timelocked behavioral
+        events.
+
+        CREATOR
+            William (Jake) Wright 10/11/2021
+
+        USAGE
+            roi_stim_epochs, roi_mean_sems = get_trace_mean_sem(inputs)
+
+        INPUT PARAMETERS
+            activity - dataframe of neural activity, with each column
+                       corresponding to each ROI
+
+            timestamps - a list of timestamps corresponding to the imaging
+                         frame where each behavioral event occured
+
+            window - list specifying the time before and after the behavioral
+                     event you want to assess (e.g. [-2,2] for 2 secs before
+                     and after.
+
+            sampling_rate - scaler specifying the image sampling rate. Default
+                            is set to 30hz.
+
+        OUTPUT PARAMETERS
+            roi_stim_epochs - dictionary containing an array for each roi. Each array
+                              contains the activity during the window for each behavioral
+                              event, with a column for each event
+
+            roi_mean_sems - dictionary containing the activity mean and sem for each ROI.
+                            For each ROI key there are two lists, one for the mean activity
+                            during the mean, and the other for the sem during the same
+                            period. '''
+
+
+    # first get the window size
+    before = window[0]*sampling_rate
+    after = window[1]*sampling_rate
+    win_size = -before + after
+    roi_stim_epochs = {}
+    for col in activity.columns:
+        d = activity[col]
+        epochs = np.zeros(win_size).reshape(-1,1)
+        for i in timestamps:
+            e = np.array(d[i+before:i+after]).reshape(-1,1)
+            epochs = np.hstack((epochs,e))
+        epochs = epochs[:,1:]
+        roi_stim_epochs[col] = epochs
+
+    # Get mean and sem of the traces
+    roi_mean_sems = {}
+    for key,value in roi_stim_epochs.items():
+        m = np.mean(value,axis=1)
+        sem = stats.sem(value,axis=1)
+        roi_mean_sems[key] = [m,sem]
+
+    return roi_stim_epochs, roi_mean_sems
+
