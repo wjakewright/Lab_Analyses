f4c6768ba5738d74137ca745d6dcf2a5886b5115
Jake Wright <wrightwj8@gmail.com>
2022-03-07 15:59:22 -0800

    Added session lever press plotting

--- Lab_Analyses/Behavior/behavior_plotting.py
+++ Lab_Analyses/Behavior/behavior_plotting.py
@@ -13,7 +13,46 @@ sns.set()
 sns.set_style("ticks")
 
 
-def plot_session_rewarded_lever_presses(movement_matrix, movement_avg):
+def plot_session_rewarded_lever_presses(mouse_lever_data, session, figsize=(8,5), x_lim=None, y_lim=None):
     """Function to plot each rewarded lever press as well as the 
-        average rewarded lever press"""
+        average rewarded lever press
+        
+        INPUT PARAMETERS
+            mosue_lever_data - Mouse_Lever_Data dataclass object containing all the 
+                                lever press data for a single mouse
+            
+            session - int specifying which session you wish to plot
+
+            figsize - tuple specifying the size to make the figure. Default set to 8 x 5
+
+            x_lim - tuple specifying what to make the xlim (left, right). Optional
+
+            y_lim - tuple specifying what to make the ylim (bottom, top). Optional
+    """
+
+    # Set title
+    title = f"{mouse_lever_data.mouse_id} from session {session}"
+
+    # Make figure
+    fig = plt.figure(figsize=figsize)
+    fig.suptitle(title)
+
+    # Plot individual movments
+    move_mat = mouse_lever_data.all_movements[session-1]
+    for row, _ in enumerate(move_mat[:,0]):
+        plt.plot(move_mat[row,:], color="gray", linewidth=0.5, alpha=0.3)
+    
+    # Plot average movement
+    avg_move = mouse_lever_data.average_movements[session-1]
+    plt.plot(avg_move, color="black", linewidth=1)
+
+    if x_lim is not None:
+        plt.xlim(left=x_lim[0], right=x_lim[1])
+    if y_lim is not None:
+        plt.ylim(bottom=y_lim[0], top=y_lim[1])
+    
+
+
+    
+
 
