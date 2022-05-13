d00605dea220419fc80f8f93ad385ad1cbaca159
Jake Wright <wrightwj8@gmail.com>
2022-05-13 13:14:54 -0700

    initial commit

---
+++ Lab_Analyses/Utilities/matlab_smooth.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def matlab_smooth(data, window):
+    """Helper function to replication the implementation of matlab smooth function
+    
+        data = 1d np.array
+        
+        window = int. Must be odd value
+    """
+    out0 = np.convolve(data, np.ones(window, dtype=int), "valid") / window
+    r = np.arange(1, window - 1, 2)
+    start = np.cumsum(data[: window - 1])[::2] / r
+    stop = (np.cumsum(data[:-window:-1])[::2] / r)[::1]
+
+    return np.concatenate((start, out0, stop))
