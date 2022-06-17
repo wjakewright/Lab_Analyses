f96fce71cc0d0f657c0f4469b5ccb05182282671
Jake Wright <wrightwj8@gmail.com>
2022-06-17 11:21:50 -0700

    Finished and fixed bugs

--- Lab_Analyses/Optogenetics/load_opto_sessions.py
+++ Lab_Analyses/Optogenetics/load_opto_sessions.py
@@ -19,12 +19,18 @@ def load_opto_sessions(mice, days, keywords=None):
             keywords - a list of keywords that should be in the file names to be loaded
             
         OUTPUT PARAMETERS
+            imaging_datasets - dictionary for each session that contains the loaded files
+                                for that session 
+            
+            behavior_datsets - dictionary for each session that contains the loaded files
+                                for that session
     
     """
     initial_path = r"C:\Users\Jake\Desktop\Analyzed_data\individual"
 
     # Initialize the output
-    loaded_files = []
+    imaging_datasets = {}
+    behavior_datasets = {}
 
     # Go through each mouses data
     for mouse, day in zip(mice, days):
@@ -65,20 +71,37 @@ def load_opto_sessions(mice, days, keywords=None):
                     session_behavior_files.append(sub_b_files)
 
             # see if there are multiple subsession for each session using different powers
-            for image_files, behavior_files in zip(
-                session_imaging_files, session_behavior_files
+            for image_files, behavior_files, name in zip(
+                session_imaging_files, session_behavior_files, session_names
             ):
+                if not image_files:
+                    continue
+
                 if len(image_files) > 1:
                     sub_sessions = []
+                    i_files = []
+                    b_files = []
                     for i in image_files:
                         sub = re.search("[0-9]+mw", i).group()
                         sub_sessions.append(sub)
 
+                    try:
+                        sub_sessions.sort(key=lambda x: int(x[:-2]))
+                    except ValueError:
+                        pass
                     for sub_sess in sub_sessions:
                         i_fname = [x for x in image_files if f"_{sub_sess}" in x]
                         b_fname = [x for x in behavior_files if f"_{sub_sess}" in x]
-                        i_file = load_pickle(i_fname, i_path)
-                        b_file = load_pickle(b_fname, b_path)
+                        i_fname = i_fname[0].split(".")[0]
+                        b_fname = b_fname[0].split(".")[0]
+                        i_file = load_pickle([i_fname], i_path)[0]
+                        b_file = load_pickle([b_fname], b_path)[0]
+                        i_files.append(i_file)
+                        b_files.append(b_file)
                 else:
-                    i_file = load_pickle(image_files, i_path)
-                    b_file = load_pickle(behavior_files, b_path)
+                    i_files = load_pickle(image_files, i_path)[0]
+                    b_files = load_pickle(behavior_files, b_path)[0]
+                imaging_datasets[name] = i_files
+                behavior_datasets[name] = b_files
+
+    return imaging_datasets, behavior_datasets
