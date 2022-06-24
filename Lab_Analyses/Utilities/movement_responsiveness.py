03c24304cb2ac81bd81d425113b2f20118abe070
Jake Wright <wrightwj8@gmail.com>
2022-06-24 10:47:52 -0700

    Changed lever chunking method

--- Lab_Analyses/Utilities/movement_responsiveness.py
+++ Lab_Analyses/Utilities/movement_responsiveness.py
@@ -1,5 +1,7 @@
 """Module for determining if an ROI displays movement-related activity"""
 
+import random
+
 import numpy as np
 
 
@@ -9,7 +11,7 @@ def movement_responsiveness(dFoF, active_lever, permutations=10000):
         INPUT PARAMTERS
             dFoF - 2d np.array of the dFoF activity, with each ROI in a column
             
-            active_lever - 1d np.array of the binarized lever trace 
+            active_leer - 1d np.array of the binarized lever trace 
             
             permutations - int specifying how many shuffles of the data to perform
                             Default is set to 10,000 shuffles
@@ -21,6 +23,18 @@ def movement_responsiveness(dFoF, active_lever, permutations=10000):
     movement_related = []
     movement_activities = {"Real": [], "Shuffled": []}
 
+    # Get the movement durations and inter-movement durations for shuffling
+    ## Get the starts and stops of each movement
+    pad = np.zeros(1)
+    boundary_frames = np.diff(np.concatenate((pad, active_lever, pad))) != 0
+    boundary_frames = np.nonzero(boundary_frames)[0]
+    active_lever_splits = []
+    for i in boundary_frames[1:]:
+        start = boundary_frames[i - 1]
+        stop = boundary_frames[i]
+        split = active_lever[start : stop + 1]
+        active_lever_splits.append(split)
+
     # Analyze each ROI
     for i in range(dFoF.shape[1]):
         activity = dFoF[:, i].reshape(-1, 1)  # Make it a column vector
@@ -29,10 +43,9 @@ def movement_responsiveness(dFoF, active_lever, permutations=10000):
         # Perform shuffles
         shuffled_activity = []
         for j in range(permutations):
-            # Get the movement durations and inter-movement durations
-            ## Get the starts and stops of each movement
-            pad = np.zeros(1)
-            move_starts = np.diff(np.concatenate((pad, active_lever, pad))) == 1
-            move_starts = np.nonzero(move_starts)[0]
-            move_stops = np.diff(np.concatenate((pad, active_lever, pad))) == -1
-            move_stops = np.nonzero(move_stops)[0]
+            # Shuffle the movement epochs
+            shuffled_splits = random.shuffle(active_lever_splits)
+            shuffled_active_lever = np.concatenate((shuffled_splits))
+            shuffled_activity.append(np.dot(activity, shuffled_active_lever))
+
+        # Assess significance
