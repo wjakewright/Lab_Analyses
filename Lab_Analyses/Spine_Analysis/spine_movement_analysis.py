413e46846873438d9f8fe3d61d977727af53e0e2
Jake Wright <wrightwj8@gmail.com>
2022-07-22 14:34:34 -0700

    initial commit

---
+++ Lab_Analyses/Spine_Analysis/spine_movement_analysis.py
@@ -0,0 +1,5 @@
+"""Module to handle some of the movement analysis for the spine activity data"""
+
+import numpy as np
+from Lab_Analyses.Spine_Analysis.spine_utilities import load_spine_datasets
+from Lab_Analyses.Utilities import data_utilities as d_utils
