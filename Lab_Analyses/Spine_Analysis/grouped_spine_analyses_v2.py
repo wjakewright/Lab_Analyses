81fcfed65699635570a66c3b558f9f7c8b1b19a9
Jake Wright <wrightwj8@gmail.com>
2022-07-25 15:01:39 -0700

    Added coactivity analysis

--- Lab_Analyses/Spine_Analysis/grouped_spine_analyses_v2.py
+++ Lab_Analyses/Spine_Analysis/grouped_spine_analyses_v2.py
@@ -2,6 +2,7 @@ import os
 from collections import defaultdict
 
 import numpy as np
+from Lab_Analyses.Spine_Analysis.global_coactivity import global_coactivity_analysis
 from Lab_Analyses.Spine_Analysis.spine_utilities import load_spine_datasets
 from Lab_Analyses.Spine_Analysis.structural_plasticity import (
     calculate_volume_change,
@@ -206,3 +207,129 @@ def movement_spine_volume(
         mouse_depressed["nonmovement"].append(sum(non_dep) / len(non_dep))
 
     return total_data, movement_volumes, mouse_means, mouse_potentiated, mouse_depressed
+
+
+def coactivity_spine_volume(
+    mice_list, day, corrected=True, threshold=0.25, exclude="Shaft Spine",
+):
+    """Function to compare the analysis spine volume and co-activity. Combines
+        data across FOVs and mice
+        
+        INPUT PARAMETERS
+            mice_list - list of str of mice to load
+            
+            day - str specifying what sessions you wish to load. Used to find
+                the file names
+                
+            corrected - boolean specifying if you wish to use the corrected spine
+                        volume or not
+                        
+            threshold - float specifying the threshold used to classify plastic spines
+            
+            exclude - str specifying spine type to exclude from analysis
+    """
+
+    grouped_data = defaultdict(list)
+    merged_group_data = defaultdict(list)
+    for mouse in mice_list:
+        print(mouse)
+        mouse_datasets = load_spine_datasets(mouse, [day], followup=True)
+        mouse_data = defaultdict(list)
+
+        for data in mouse_datasets.values():
+            keys = list(data.keys())
+            datasets = list(data.values())
+
+            spine_groupings = datasets[0].spine_grouping
+            # Get the coactivity results
+            (
+                global_correlation,
+                coactivity_rate,
+                spine_fraction_coactive,
+                dend_fraction_coactive,
+                coactive_amplitude,
+                coactive_spines,
+                coactivity_epoch_trace,
+                coactivity_mean_trace,
+                dend_mean_sem,
+            ) = global_coactivity_analysis(datasets[0], sampling_rate=60)
+
+            # convert coactivity_mean_trace dict to list
+            coactivity_mean_trace = list(coactivity_mean_trace.values())
+            coactivity_epoch_trace = list(coactivity_epoch_trace.values())
+
+            # Get the spine volumes
+            if corrected:
+                _, volumes, spine_idxs = calculate_volume_change(
+                    datasets, keys, exclude=exclude,
+                )
+            else:
+                volumes, _, spine_idxs = calculate_volume_change(
+                    datasets, keys, exclude=exclude,
+                )
+            # Use only the post volumes
+            volumes = volumes[keys[1]]
+
+            # classify plasticity
+            potentiated, depressed, _ = classify_plasticity(volumes, threshold)
+
+            # Get the coactivity only for the stable spines
+            global_correlation = global_correlation[spine_idxs]
+            coactivity_rate = coactivity_rate[spine_idxs]
+            spine_fraction_coactive = spine_fraction_coactive[spine_idxs]
+            dend_fraction_coactive = dend_fraction_coactive[spine_idxs]
+            coactive_amplitude = coactive_amplitude[spine_idxs]
+            coactive_spines = coactive_spines[spine_idxs]
+            groupings = []
+            if type(spine_groupings[0]) == list:
+                for grouping in spine_groupings:
+                    g = [x for x in grouping if x in spine_idxs]
+                    groupings.append(g)
+            else:
+                groupings.append(spine_groupings)
+            coactivity_epoch_trace = [coactivity_epoch_trace[i] for i in spine_idxs]
+            coactivity_mean_trace = [coactivity_mean_trace[i] for i in spine_idxs]
+
+            # store values
+            mouse_data["volumes"].append(volumes)
+            mouse_data["potentiated"].append(potentiated)
+            mouse_data["depressed"].append(depressed)
+            mouse_data["correlation"].append(global_correlation)
+            mouse_data["coactivity_rate"].append(coactivity_rate)
+            mouse_data["spine fraction"].append(spine_fraction_coactive)
+            mouse_data["dend fraction"].append(dend_fraction_coactive)
+            mouse_data["coactive amplitude"].append(coactive_amplitude)
+            mouse_data["coactive spines"].append(coactive_spines)
+            mouse_data["epoch traces"].append(coactivity_epoch_trace)
+            mouse_data["mean traces"].append(coactivity_mean_trace)
+            mouse_data["dend traces"].append(dend_mean_sem)
+            mouse_data["grouping"].append(groupings)
+
+        # Merge mouse data across FOVs
+        merged_mouse_data = {}
+        for key, value in mouse_data.items():
+            if (
+                key == "grouping"
+                or key == "dend traces"
+                or key == "mean traces"
+                or key == "epoch traces"
+            ):
+                continue
+            if len(value) == 1:
+                merged_mouse_data[key] = value[0]
+                continue
+            merged_mouse_data[key] = np.concatenate(value)
+
+        # store data for this mouse
+        for key, value in mouse_data.items():
+            grouped_data[key].append(value)
+        for key, value in merged_mouse_data.items():
+            merged_group_data[key].append(value)
+
+    # Merge data across mice
+    total_data = {}
+    for key, value in merged_group_data.items():
+        total_data[key] = np.concatenate(value)
+
+    return total_data, grouped_data
+
