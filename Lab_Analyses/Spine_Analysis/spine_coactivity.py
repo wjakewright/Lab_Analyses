b02f390250d86481ba106974618d388e647e117f
Jake Wright <wrightwj8@gmail.com>
2022-08-04 10:47:32 -0700

    initial commit

---
+++ Lab_Analyses/Spine_Analysis/spine_coactivity.py
@@ -0,0 +1,96 @@
+"""Module to perform spine co-activity analyses"""
+
+import numpy as np
+from scipy import stats
+
+
+def spine_coactivity_analysis(
+    spine_activity, spine_positions, spine_grouping, bin_size=5, sampling_rate=60,
+):
+    """Function to calculate pairwise spine coactivity rate between 
+        all spines along the same dendrite. Spine rates are then binned
+        based on their distance from the target spine
+        
+        INPUT PARAMETERS
+            spine_activity - np.array of the binarized spine activity traces
+                            Each column represents each spine
+            
+            spine_positions - list of the corresponding spine positions along the dendrite
+                              for each spine
+            
+            spine_grouping - list with the corresponding groupings of spines on
+                             the same dendrite
+            
+            bin_size - int or float specifying the distance to bin over
+            
+    """
+    # Set up the position bins
+    MAX_DIST = 30
+    bin_num = MAX_DIST / bin_size
+    posistion_bins = np.linspace(0, MAX_DIST, bin_num)
+
+    # First sort out spine_groping to make sure you can iterate
+    if type(spine_grouping[0]) != list:
+        spine_grouping = [spine_grouping]
+
+    coactivity_matrix = np.zeros(bin_num, spine_activity.shape[1])
+
+    # Now iterate through each dendrite grouping
+    for spines in spine_grouping:
+        s_activity = spine_activity[:, spines]
+        positions = spine_positions[:, spines]
+
+        # Go through each spine
+        for i in range(s_activity.shape[0]):
+            current_coactivity = []
+            curr_spine = s_activity[:, i]
+            # Get coactivity with each other spine
+            for j in range(s_activity.shape[0]):
+                # Don't compare spine to itself
+                if j == i:
+                    continue
+                test_spine = s_activity[:, j]
+                co_rate = calculate_coactivity(curr_spine, test_spine, sampling_rate)
+                current_coactivity.append(co_rate)
+
+        # Order by positions
+        curr_pos = positions[i]
+        pos = [x for idx, x in enumerate(positions) if idx != i]
+        # normalize  distances relative to current position
+        relative_pos = np.array(pos) - curr_pos
+        # Make all distances positive
+        relative_pos = np.absolute(relative_pos)
+        # Sort coactivity and position based on distance
+        sorted_coactivity = [
+            x for _, x in sorted(zip(relative_pos, current_coactivity))
+        ]
+        sorted_positions = [y for y, _ in sorted(zip(relative_pos, current_coactivity))]
+        # Bin the data
+        binned_coactivity = [
+            sorted_coactivity[
+                np.where((sorted_positions > low) & sorted_positions <= high)
+            ]
+            for low, high in zip(posistion_bins[:-1], posistion_bins[1:])
+        ]
+        coactivity_matrix[:, spines[i]] = np.array(binned_coactivity).reshape(-1, 1)
+
+    return coactivity_matrix
+
+
+def calculate_coactivity(spine_1, spine_2, sampling_rate):
+    """Helper function to calculate spine coactivity rate"""
+    duration = len(spine_1) / sampling_rate
+    coactivity = spine_1 * spine_2
+    events = np.nonzero(np.diff(coactivity) == 1)[0]
+    event_num = len(events)
+    event_freq = event_num / duration
+
+    # Normalize frequency based on overall activity levels
+    spine_1_freq = len(np.nonzero(np.diff(spine_1) == 1)[0]) / duration
+    spine_2_freq = len(np.nonzero(np.diff(spine_2) == 1)[0]) / duration
+    geo_mean = stats.gmean([spine_1_freq, spine_2_freq])
+
+    coactivity_rate = event_freq / geo_mean
+
+    return coactivity_rate
+
