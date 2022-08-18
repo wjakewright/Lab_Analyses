a50825874e57e55a5f9b52a2a07e0c69d980bbf4
Jake Wright <wrightwj8@gmail.com>
2022-08-17 22:28:07 -0700

    Added spine dynamics function

--- Lab_Analyses/Spine_Analysis/structural_plasticity.py
+++ Lab_Analyses/Spine_Analysis/structural_plasticity.py
@@ -3,8 +3,10 @@
 from itertools import compress
 
 import numpy as np
-from Lab_Analyses.Spine_Analysis.spine_utilities import (find_spine_classes,
-                                                         find_stable_spines)
+from Lab_Analyses.Spine_Analysis.spine_utilities import (
+    find_spine_classes,
+    find_stable_spines,
+)
 
 
 def calculate_volume_change(data_list, days=None, exclude=None):
@@ -60,7 +62,7 @@ def calculate_volume_change(data_list, days=None, exclude=None):
         rel_corr_vol = np.array(corr_vol) / np.array(baseline_corr_vol)
         relative_volume[day] = rel_vol
         relative_corrected_volume[day] = rel_corr_vol
-    
+
     # Get the spine indexes
     spine_idxs = np.nonzero(stable_spines)[0]
 
@@ -97,4 +99,53 @@ def classify_plasticity(relative_volumes, threshold=0.25):
             stable_spines[i] = True
 
     return potentiated_spines, depressed_spines, stable_spines
+
+
+def calculate_spine_dynamics(data_list, days=None, exclude=None):
+    """Function to calculate the spine density and rate of spine 
+        generation and elimination
+        
+        INPUT PARAMETERS
+            data_list - list of datasets to compare each other to. All datasets will
+                        be compared to the first dataset. This requires data to have both
+                        lists of spine volume and spine flags. 
+            
+            days - list of str specifying which day each dataset corresponds to.
+                    Default is none, which will automatically generate labels
+
+            exclude - str specifying a type of spine to exclude from the analysis
+
+        OUTPUT PARAMETERS
+            spine_density - dict containing spine density for each day
+
+            fraction_new_spines - dict containing fraction of new spines for each day
+
+            fraction_eliminated_spines - dict containing fraction of eliminated spines for each day
+    
+    """
+    # Set up days
+    if days is None:
+        days = [f"Day {x}" for x in range(1, len(data_list) + 1)]
+
+    # Set up the spine flags, and make sure they are the same length
+    flag_list = [data.spine_flags for data in data_list]
+    # Get the new spines and eliminated spines for each day
+    new_spine_list = []
+    eliminated_spine_list = []
+    for flags in flag_list:
+        new_spine_list.append(find_spine_classes(flags, "New Spine"))
+        eliminated_spine_list.append(find_spine_classes(flags, "Eliminated Spine"))
+
+    # Set up outputs
+    spine_density = {}
+    fraction_new_spines = {}
+    fraction_eliminated_spines = {}
+
+    # Analyze each day
+    for i, (day, dataset) in enumerate(zip(days, data_list)):
+        groupings = dataset.spine_grouping
+        if type(groupings) != list:
+            groupings = [groupings]
+        for dendrite in groupings:
+            length = dataset.spine_positions[dendrite[-1]]
 
