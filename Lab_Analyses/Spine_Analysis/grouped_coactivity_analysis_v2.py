b19c4230b453d192333e04c77c85bccc7d1063f4
Jake Wright <wrightwj8@gmail.com>
2022-09-26 10:23:00 -0700

    Added analyses for loca, total, and conj coactivity

---
+++ Lab_Analyses/Spine_Analysis/grouped_coactivity_analysis_v2.py
@@ -0,0 +1,181 @@
+import os
+from collections import defaultdict
+from dataclasses import dataclass
+
+import numpy as np
+from Lab_Analyses.Spine_Analysis.conjunctive_spine_coactivity import (
+    conjunctive_coactivity_analysis,
+)
+from Lab_Analyses.Spine_Analysis.local_spine_coactivity import (
+    local_spine_coactivity_analysis,
+)
+from Lab_Analyses.Spine_Analysis.spine_movement_analysis import (
+    quantify_movment_quality,
+    spine_movement_activity,
+)
+from Lab_Analyses.Spine_Analysis.spine_utilities import load_spine_datasets
+from Lab_Analyses.Spine_Analysis.total_spine_coactivity import total_coactivity_analysis
+from Lab_Analyses.Utilities.save_load_pickle import save_pickle
+
+
+def grouped_coactivity_analysis(
+    mice_list,
+    day,
+    followup=True,
+    movement_epochs=None,
+    zscore=False,
+    volume_norm=False,
+    save=False,
+    save_path=None,
+):
+    """Function to handle coactivity analysis of dual spine imaging datsets across all
+        mice and all FOVs. Stores coactivity data in dataclass to be used in subsequent
+        analyses (e.g., plasticity)
+        
+        INPUT PARAMETERS
+            mice_list - list of strings specifying mice ids to be analyzed
+            
+            day - string of the day to analyze
+            
+            followup - boolean specifying if you want to include followup structural 
+                        imaging sessions
+            
+            movment_epochs - str specifying if you want to analyze only during movements and
+                            what type of movements. Accepts 'movement', 'rewarded', 
+                            'unrewarded', 'nonmovement' and 'learned'. Default is None to 
+                            analyze the entire imaging period
+            
+            zscore - boolean of whether or not to zscore activity for analysis
+                            
+            save - boolean of whether to save the output or not
+            
+            save_path - str specifying where to save the data
+            
+    """
+
+    grouped_data = defaultdict(list)
+
+    # Analyze each mouse seperately
+    for mouse in mice_list:
+        print(f"--- Analyzing {mouse}")
+        mouse_data = defaultdict(list)
+        if followup is True:
+            datasets = load_spine_datasets(mouse, [day], followup=True)
+        else:
+            datasets = load_spine_datasets(mouse, [day], followup=False)
+
+        # Analyze each FOV seperately
+        for FOV, dataset in datasets.items():
+            if followup is True:
+                data = dataset[f"Pre {day}"]
+                followup_volume = dataset[f"Post {day}"].corrected_spine_volume
+            else:
+                data = dataset[day]
+                followup_volume = None
+            sampling_rate = data.imaging_parameters["Sampling Rate"]
+            # Analyze local spine coactivity
+            (
+                local_distance_coactivity_rate,
+                local_distance_bins,
+                local_spine_correlation,
+                local_coactivity_rate,
+                local_coactivity_matrix,
+                local_spine_fraction_coactive,
+                local_coactive_spine_num,
+                local_coactive_spine_volumes,
+                local_spine_coactive_amplitude,
+                local_nearby_coactive_amplitude,
+                local_spine_coactive_calcium,
+                local_nearby_coactive_calcium,
+                local_spine_coactive_std,
+                local_nearby_coactive_std,
+                local_spine_coactive_calcium_std,
+                local_nearby_coactive_calcium_std,
+                local_spine_coactive_calcium_auc,
+                local_nearby_coactive_calcium_auc,
+                local_pine_coactive_traces,
+                local_nearby_coactive_traces,
+                local_spine_coactive_calcium_traces,
+                local_nearby_coactive_calcium_traces,
+            ) = local_spine_coactivity_analysis(
+                data,
+                movement_epoch=movement_epochs,
+                cluster_dist=10,
+                sampling_rate=sampling_rate,
+                zscore=zscore,
+                volume_norm=volume_norm,
+            )
+
+            # Analyze global spine-dendrite coactivity
+            (
+                global_correlation,
+                global_coactivity_event_num,
+                global_coactivity_event_rate,
+                global_spine_fraction_coactive,
+                global_dend_fraction_coactive,
+                global_spine_coactive_amplitude,
+                global_dend_coactive_amplitude,
+                global_spine_coactive_calcium,
+                global_spine_coactive_std,
+                global_dend_coactive_std,
+                global_spine_coactive_calcium_std,
+                global_dend_coactive_auc,
+                global_spine_coactive_calcium_auc,
+                global_relative_spine_coactive_amplitude,
+                global_relative_dend_coactive_amplitude,
+                global_relative_spine_coactive_calcium,
+                global_relative_spine_onsets,
+                global_dend_triggered_spine_traces,
+                global_dend_triggered_dend_traces,
+                global_dend_triggered_spine_calcium_traces,
+                global_coactive_spine_traces,
+                global_coactive_dend_traces,
+                global_coactive_spine_calcium_traces,
+                global_coactivity_matrix,
+            ) = total_coactivity_analysis(
+                data,
+                movement_epoch=movement_epochs,
+                sampling_rate=sampling_rate,
+                zscore=zscore,
+                volume_norm=volume_norm,
+            )
+
+            # Analyze conjunctive local spine and dendrite coactivity
+            (
+                conjunctive_correlation,
+                conj_coactivity_event_num,
+                conj_coactivity_event_rate,
+                conj_spine_fraction_coactive,
+                conj_dend_fraction_coactive,
+                conj_coactive_spine_num,
+                conj_coactive_spine_volumes,
+                conj_spine_coactive_amplitude,
+                conj_nearby_coactive_amplitude_sum,
+                conj_spine_coactive_calcium,
+                conj_nearby_coactive_calcium_sum,
+                conj_dend_coactive_amplitude,
+                conj_spine_coactive_std,
+                conj_nearby_coactive_std,
+                conj_spine_coactive_calcium_std,
+                conj_nearby_coactive_calcium_std,
+                conj_dend_coactive_std,
+                conj_spine_coactive_calcium_auc,
+                conj_nearby_coactive_calcium_auc_sum,
+                conj_dend_coactive_auc,
+                conj_relative_spine_dend_onsets,
+                conj_coactive_spine_traces,
+                conj_coactive_nearby_traces,
+                conj_coactive_spine_calcium_traces,
+                conj_coactive_nearby_calcium_traces,
+                conj_coactive_dend_traces,
+                conj_conjunctive_coactivity_matrix,
+            ) = conjunctive_coactivity_analysis(
+                data,
+                movement_epoch=movement_epochs,
+                cluster_dist=10,
+                sampling_rate=sampling_rate,
+                zscore=zscore,
+                volume_norm=volume_norm,
+            )
+
+            # Analyze movement
