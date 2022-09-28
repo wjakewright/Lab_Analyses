08a08dec7798a99c3e2f3749056581e7b1fdf7df
Jake Wright <wrightwj8@gmail.com>
2022-09-28 11:37:25 -0700

    Added spine and dend frequency calculations and made dataclass for output

--- Lab_Analyses/Spine_Analysis/grouped_coactivity_analysis_v2.py
+++ Lab_Analyses/Spine_Analysis/grouped_coactivity_analysis_v2.py
@@ -69,9 +69,11 @@ def grouped_coactivity_analysis(
             if followup is True:
                 data = dataset[f"Pre {day}"]
                 followup_volume = dataset[f"Post {day}"].corrected_spine_volume
+                followup_flags = dataset[f"Post {day}"].spine_flags
             else:
                 data = dataset[day]
                 followup_volume = None
+                followup_flags = None
             sampling_rate = data.imaging_parameters["Sampling Rate"]
             # Analyze local spine coactivity
             (
@@ -280,4 +282,136 @@ def grouped_coactivity_analysis(
                 threshold=0.5,
                 sampling_rate=sampling_rate,
             )
+
+            # Get basic activity frequencies
+            spine_activity_freq = []
+            dend_activity_freq = []
+            for s in range(data.spine_GluSnFr_activity.shape[1]):
+                duration = len(data.spine_GluSnFr_activity[:, s]) / sampling_rate
+                s_events = np.nonzero(np.diff(data.spine_GluSnFr_activity[:, s]) == 1)
+                d_events = np.nonzero(np.diff(dend_activity_matrix[:, s]) == 1)
+                spine_freq = (s_events / duration) * 60
+                dend_freq = (d_events / duration) * 60
+                spine_activity_freq.append(spine_freq)
+                dend_activity_freq.append(dend_freq)
+            spine_activity_freq = np.array(spine_activity_freq)
+            dend_activity_freq = np.array(dend_activity_freq)
+
+            fovs = [FOV for i in range(spine_activity_freq)]
+            ids = [mouse for i in range(spine_activity_freq)]
+
+            # Store data from this mouse in mouse_data dictionary
+
+
+################# DATACLASS #####################
+@dataclass
+class Spine_Coactivity_Data:
+    """Dataclass for storing spine data of a single day following coactivity analysis 
+        across all mice in a given group"""
+
+    mouse_id: list
+    FOV: list
+    parameters: dict
+    spine_flags: list
+    followup_flags: list
+    spine_volumes: np.array
+    followup_volumes: np.array
+    spine_activity_freq: np.array
+    dend_activity_freq: np.array
+    distance_coactivity_rate: np.array  # 2d
+    local_spine_correlation: np.array
+    local_coactivity_rate: np.array
+    local_spine_fraction_coactive: np.array
+    local_coactive_spine_num: np.array
+    local_coactive_spine_volumes: np.array
+    local_spine_coactive_amplitude: np.array
+    local_nearby_coactive_amplitude: np.array
+    local_spine_coactive_calcium: np.array
+    local_nearby_coactive_calcium: np.array
+    local_spine_coactive_std: np.array
+    local_nearby_coactive_std: np.array
+    local_spine_coactive_calcium_std: np.array
+    local_nearby_coactive_calcium_std: np.array
+    local_spine_coactive_calcium_auc: np.array
+    local_nearby_coactive_calcium_auc: np.array
+    local_pine_coactive_traces: list
+    local_nearby_coactive_traces: list
+    local_spine_coactive_calcium_traces: list
+    local_nearby_coactive_calcium_traces: list
+    global_correlation: np.array
+    global_coactivity_event_num: np.array
+    global_coactivity_event_rate: np.array
+    global_spine_fraction_coactive: np.array
+    global_dend_fraction_coactive: np.array
+    global_spine_coactive_amplitude: np.array
+    global_dend_coactive_amplitude: np.array
+    global_spine_coactive_calcium: np.array
+    global_spine_coactive_std: np.array
+    global_dend_coactive_std: np.array
+    global_spine_coactive_calcium_std: np.array
+    global_dend_coactive_auc: np.array
+    global_spine_coactive_calcium_auc: np.array
+    global_relative_spine_coactive_amplitude: np.array
+    global_relative_dend_coactive_amplitude: np.array
+    global_relative_spine_coactive_calcium: np.array
+    global_relative_spine_onsets: np.array
+    global_dend_triggered_spine_traces: list
+    global_dend_triggered_dend_traces: list
+    global_dend_triggered_spine_calcium_traces: list
+    global_coactive_spine_traces: list
+    global_coactive_dend_traces: list
+    global_coactive_spine_calcium_traces: list
+    conjunctive_correlation: np.array
+    conj_coactivity_event_num: np.array
+    conj_coactivity_event_rate: np.array
+    conj_spine_fraction_coactive: np.array
+    conj_dend_fraction_coactive: np.array
+    conj_coactive_spine_num: np.array
+    conj_coactive_spine_volumes: np.array
+    conj_spine_coactive_amplitude: np.array
+    conj_nearby_coactive_amplitude_sum: np.array
+    conj_spine_coactive_calcium: np.array
+    conj_nearby_coactive_calcium_sum: np.array
+    conj_dend_coactive_amplitude: np.array
+    conj_spine_coactive_std: np.array
+    conj_nearby_coactive_std: np.array
+    conj_spine_coactive_calcium_std: np.array
+    conj_nearby_coactive_calcium_std: np.array
+    conj_dend_coactive_std: np.array
+    conj_spine_coactive_calcium_auc: np.array
+    conj_nearby_coactive_calcium_auc_sum: np.array
+    conj_dend_coactive_auc: np.array
+    conj_relative_spine_dend_onsets: np.array
+    conj_coactive_spine_traces: list
+    conj_coactive_nearby_traces: list
+    conj_coactive_spine_calcium_traces: list
+    conj_coactive_nearby_calcium_traces: list
+    conj_coactive_dend_traces: list
+    move_dend_traces: list
+    move_spine_traces: list
+    move_dend_amplitude: np.array
+    move_dend_std: np.array
+    move_spine_amplitude: np.array
+    move_spine_std: np.array
+    move_dend_onset: np.array
+    move_spine_onset: np.array
+    rwd_move_dend_traces: list
+    rwd_move_spine_traces: list
+    rwd_move_dend_amplitude: np.array
+    rwd_move_dend_std: np.array
+    rwd_move_spine_amplitude: np.array
+    rwd_move_spine_std: np.array
+    rwd_move_dend_onset: np.array
+    rwd_move_spine_onset: np.array
+    learned_movement: np.array
+    spine_movements: list
+    spine_movement_corr: np.array
+    dend_movements: list
+    dend_movement_corr: np.array
+    local_movements: list
+    local_movement_corr: np.array
+    global_movements: list
+    global_movement_corr: np.array
+    conj_movements: list
+    conj_movement_corr: np.array
 
