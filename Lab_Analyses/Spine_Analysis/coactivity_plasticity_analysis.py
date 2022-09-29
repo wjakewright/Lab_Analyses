229975b95ac9eaa1e41492269be23738d1cd99cb
Jake Wright <wrightwj8@gmail.com>
2022-09-29 15:50:56 -0700

    Added method to calculate spine volume changes

--- Lab_Analyses/Spine_Analysis/coactivity_plasticity_analysis.py
+++ Lab_Analyses/Spine_Analysis/coactivity_plasticity_analysis.py
@@ -35,3 +35,55 @@ class Coactivity_Plasticity:
             else:
                 raise Exception("Data must have followup data containing spine volumes")
 
+        self.day = self.dataset.day
+        self.threshold = threshold
+        self.exclude = exclude
+        self.parameters = self.dataset.parameters
+
+        # Analyze the data
+        self.analyze_plasticity()
+
+    def analyze_plasticity(self):
+        """Method to calculate spine volume change and classify plasticity"""
+
+        volume_data = [self.dataset.spine_volumes, self.followup_volumes]
+        flag_list = [self.dataset.spine_flags, self.followup_flags]
+
+        relative_volumes, spine_idxs = calculate_volume_change(
+            volume_data, flag_list, days=None, exclude=self.exclude
+        )
+        relative_volumes = list(relative_volumes.values())[0]
+        enlarged_spines, shrunken_spines, stable_spines = classify_plasticity(
+            relative_volumes, self.threshold
+        )
+
+        # Store volume change and plasticity classification
+        self.relative_volumes = relative_volumes
+        self.enlarged_spines = enlarged_spines
+        self.shrunken_spines = shrunken_spines
+        self.stable_spines = stable_spines
+
+        # Refine coactivity variables for only stable spines and store them
+        attributes = list(self.dataset.__dict__.keys())
+        ## Go through each attribute
+        for attribute in attributes:
+            if (
+                attribute == "day"
+                or attribute == "mouse_id"
+                or attribute == "parameters"
+                or attribute == "learned_movement"
+            ):
+                continue
+            # Refine the attributes data
+            variable = getattr(self.dataset, attribute)
+            if type(variable) == np.array:
+                if len(variable.shape) == 1:
+                    new_variable = variable[spine_idxs]
+                elif len(variable.shape) == 2:
+                    new_variable = variable[:, spine_idxs]
+            elif type(variable) == list:
+                new_variable = [variable[i] for i in spine_idxs]
+            else:
+                raise Exception(f"{variable} is incorrect datatype !!!")
+            # Store the attribute
+            setattr(self, attribute, new_variable)
