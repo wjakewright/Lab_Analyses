4eddcddaa70f568a2ebb17f2107fb1af28da335a
Jake Wright <wrightwj8@gmail.com>
2022-10-06 15:07:30 -0700

    fixed selection of stable spines

--- Lab_Analyses/Spine_Analysis/structural_plasticity.py
+++ Lab_Analyses/Spine_Analysis/structural_plasticity.py
@@ -3,8 +3,10 @@
 from itertools import compress
 
 import numpy as np
-from Lab_Analyses.Spine_Analysis.spine_utilities import (find_spine_classes,
-                                                         find_stable_spines)
+from Lab_Analyses.Spine_Analysis.spine_utilities import (
+    find_spine_classes,
+    find_stable_spines,
+)
 
 
 def calculate_volume_change(volume_list, flag_list, days=None, exclude=None):
@@ -33,25 +35,23 @@ def calculate_volume_change(volume_list, flag_list, days=None, exclude=None):
 
     # Get indexes of stable spines throughout all analyzed days
     stable_spines = find_stable_spines(flag_list)
-    print(stable_spines)
 
     # Find additional spines to exclude
     if exclude:
         exclude_spines = find_spine_classes(flag_list[-1], exclude)
         # Reverse values to exclude these spines
         exclude_spines = np.array([not x for x in exclude_spines])
-        print(exclude_spines)
         # Combine with the stable spines
         stable_spines = stable_spines * exclude_spines
 
+    stable_spines = stable_spines.astype(bool)
+    # Get stable spine indexes
+    stable_idxs = np.nonzero(stable_spines)[0]
     # Get volumes only for stable_spines
     spine_volumes = []
     for volumes in volume_list:
-        print(len(volumes))
-        print(len(stable_spines))
         vols = np.array(volumes)
-        stable_vols = vols[stable_spines]
-        print(len(stable_vols))
+        stable_vols = vols[stable_idxs]
         spine_volumes.append(stable_vols)
 
     # Calculate relative volume now
@@ -60,9 +60,6 @@ def calculate_volume_change(volume_list, flag_list, days=None, exclude=None):
     for vol, day in zip(spine_volumes, days):
         rel_vol = vol / baseline_volume
         relative_volumes[day] = rel_vol
-
-    # Get stable spine indexes
-    stable_idxs = np.nonzero(stable_spines)[0]
 
     return relative_volumes, stable_idxs
 
