b145011689763ac4ebc5986ee383d2413bc91f1a
Jake Wright <wrightwj8@gmail.com>
2023-01-04 13:12:11 -0800

    initial commit

---
+++ Lab_Analyses/Spine_Analysis/coactivity_plasticity_analysis_v2.py
@@ -0,0 +1,134 @@
+import os
+from itertools import compress
+
+import numpy as np
+from scipy import stats
+
+from Lab_Analyses.Spine_Analysis import spine_plotting as sp
+from Lab_Analyses.Spine_Analysis.structural_plasticity import (
+    calculate_volume_change,
+    classify_plasticity,
+)
+from Lab_Analyses.Utilities.save_load_pickle import save_pickle
+
+
+class Coactivity_Plasticity:
+    """Class to handle the analysis of spine plasticity on coactivity datasets"""
+
+    def __init__(
+        self, data, thershold, exclude, vol_norm=False, save=False, save_path=None
+    ):
+        """Initialize the class
+            
+            INPUT PARAMETERS    
+                data - Spine_Coactivity_Data object, or a list of these objects
+                
+                threshold - float specifying the threshold to consider for plastic spines
+                
+                exclude - str specifying the spine types to exclude form analysis (e.g., Shaft)
+        """
+
+        # Check number of datasets and set up data
+        if type(data) == list:
+            dataset = data[0]
+            subsequent_flags = data[1].spine_flags
+            subsequent_volumes = data[1].spine_volumes
+        elif isinstance(data, object):
+            if data.followup_volumes is not None:
+                dataset = data
+                subsequent_flags = data.followup_flags
+                subsequent_volumes = data.followup_volumes
+            else:
+                raise Exception("Data must have followup data containing spine volumes")
+
+        self.day = dataset.day
+        self.threshold = thershold
+        self.exclude = exclude
+        self.vol_norm = vol_norm
+        self.parameters = dataset.parameters
+        self.save = save
+        self.save_path = save_path
+
+        # Analyze the data
+        self.analyze_plasticity(dataset, subsequent_flags, subsequent_volumes)
+
+        if save:
+            self.save_output()
+
+    def analyze_plasticity(self, dataset, subsequent_flags, subsequent_volumes):
+        """Method to calculate spine volume change and classify plasticity"""
+
+        volume_data = [dataset.spine_volumes, subsequent_volumes]
+        flag_list = [dataset.spine_flags, subsequent_flags]
+        relative_volumes, spine_idxs = calculate_volume_change(
+            volume_data, flag_list, norm=self.vol_norm, days=None, exclude=self.exclude,
+        )
+        relative_volumes = np.array(list(relative_volumes.values())[-1])
+        enlarged_spines, shrunken_spines, stable_spines = classify_plasticity(
+            relative_volumes, self.threshold
+        )
+
+        # Store volume change and plasticity classifications
+        self.relative_volumes = relative_volumes
+        self.enlarge_spines = enlarged_spines
+        self.shrunken_spines = shrunken_spines
+        self.stable_spines = stable_spines
+
+        # Refine coactivity variables for only stable spines and store them
+        attributes = list(dataset.__dict__.keys())
+        for attribute in attributes:
+            # Save attributes that do not need to be refined
+            if attribute == "day" or attribute == "parameters":
+                variable = getattr(dataset, attribute)
+                setattr(self, attribute, variable)
+                continue
+
+            # Get the corresponding variable
+            variable = getattr(dataset, attribute)
+
+            # Skip variables that are None
+            if variable is None:
+                setattr(self, attribute, variable)
+                continue
+
+            # Refine variable based on stable spine idxs
+            if type(variable) == np.ndarray:
+                if len(variable.shape) == 1:
+                    new_variable = variable[spine_idxs]
+                elif len(variable.shape) == 2:
+                    new_variable = variable[:, spine_idxs]
+            elif type(variable) == list:
+                try:
+                    new_variable = [variable[i] for i in spine_idxs]
+                except IndexError:
+                    print(f"{attribute} is an empty list!!! Will skip.")
+                    print(variable)
+                    continue
+            else:
+                raise TypeError(f"{attribute} {type(variable)} is incorrect datatype")
+
+            # Store the attribute
+            setattr(self, attribute, new_variable)
+
+    def save_output(self):
+        """Method to save the output"""
+        if self.save_path is None:
+            save_path = r"C:\Users\Desktop\Analyzed_data\grouped"
+        else:
+            save_path = self.save_path
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+        # Set up name based on analyzis parameters
+        if self.parameters["zscore"]:
+            a_type = "zscore"
+        else:
+            a_type = "dFoF"
+        if self.parameters["Volume Norm"]:
+            norm = "norm"
+        else:
+            norm = ""
+        thresh = self.threshold
+
+        save_name = f"{self.day}_{a_type}{norm}_{thresh}_coactivity_plasticity_data"
+        save_pickle(save_name, self, save_path)
