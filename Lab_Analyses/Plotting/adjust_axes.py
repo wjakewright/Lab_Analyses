f1d220cf1349e15e019955ba8e3cf4eab6588547
Jake Wright <wrightwj8@gmail.com>
2023-05-25 11:10:37 -0700

    changed axes formatting

--- Lab_Analyses/Plotting/adjust_axes.py
+++ Lab_Analyses/Plotting/adjust_axes.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticks
+import numpy as np
 import seaborn as sns
 
 sns.set()
 sns.set_style("ticks")
 
 
-def adjust_axes(ax, minor_ticks, xtitle, ytitle, xlim, ylim, tick_len, axis_width):
+def adjust_axes(ax, minor_ticks, xtitle, ytitle,tick_len, axis_width):
     """Helper function to adjust the axes of plots"""
     # Format the axes
     ## Add minor tick marks
@@ -19,11 +20,7 @@ def adjust_axes(ax, minor_ticks, xtitle, ytitle, xlim, ylim, tick_len, axis_widt
         ax.yaxis.set_minor_locator(mticks.AutoMinorLocator(n=2))
     ## Add axes labels and set limits
     ax.set_xlabel(xtitle, labelpad=5)
-    if xlim:
-        ax.set_xlim(left=xlim[0], right=xlim[1])
     ax.set_ylabel(ytitle, labelpad=5)
-    if ylim:
-        ax.set_ylim(bottom=ylim[0], top=ylim[1])
     ## Adjust tick marks
     ax.tick_params(
         axis="both", which="major", direction="in", length=tick_len, width=axis_width
@@ -39,4 +36,25 @@ def adjust_axes(ax, minor_ticks, xtitle, ytitle, xlim, ylim, tick_len, axis_widt
     for axis in ax.spines.keys():
         ax.spines[axis].set_linewidth(axis_width)
 
+    ax.autoscale_view(tight=False)
+
     sns.despine(ax=ax)
+
+
+def get_axis_limit(lim, ticks):
+    """Helper function to set axis limits"""
+    if lim is None:
+        minimum = ticks[0]
+        maximum = ticks[-1]
+    else:
+        if lim[0] is None:
+            minimum = ticks[0]
+        else:
+            minimum = lim[0]
+        if lim[1] is None:
+            maximum = ticks[-1]
+        else:
+            maximum = lim[1]
+
+    return minimum, maximum
+
