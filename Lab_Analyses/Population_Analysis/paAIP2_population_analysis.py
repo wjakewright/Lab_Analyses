5e69700f7dc2c96767743b3b7455d27afaed9bd9
Jake Wright <wrightwj8@gmail.com>
2023-05-25 11:05:30 -0700

    added some of the initial analyses

--- Lab_Analyses/Population_Analysis/paAIP2_population_analysis.py
+++ Lab_Analyses/Population_Analysis/paAIP2_population_analysis.py
@@ -3,16 +3,15 @@ import os
 import numpy as np
 
 from Lab_Analyses.Population_Analysis import population_utilities as p_utils
+from Lab_Analyses.Population_Analysis.assess_MRN_activity import (
+    calculate_movement_encoding,
+    get_fraction_MRNs,
+)
 from Lab_Analyses.Utilities import data_utilities as d_utils
 
 
 def paAIP2_population_analysis(
-    paAIP2_mice,
-    EGFP_mice,
-    matched=False,
-    activity_window=(-2, 4),
-    save_ind=False,
-    save_grouped=False,
+    paAIP2_mice, EGFP_mice, activity_window=(-2, 4), save_ind=False, save_grouped=False,
 ):
     """Function to analyze experimental and control mice from paAIP2 population
         imaging experiments
@@ -23,9 +22,6 @@ def paAIP2_population_analysis(
             
             EGFP_mice - list of str specifying the mice in the EGFP group to be
                         analyzed
-            
-            matched - boolean specifying whether or not the data are roi matched
-                      across days
             
             activity_window - tuple specifying the window size to analyze activity around
 
@@ -38,4 +34,75 @@ def paAIP2_population_analysis(
     all_mice = paAIP2_mice + EGFP_mice
     paAIP2_data = []
     EGFP_data = []
+
+    sessions = ["Early", "Middle", "Late"]
+
+    # Start analyzing each mouse seperately
+    for mouse in all_mice:
+        print("----------------------------------------")
+        print(f"- Analyzing {mouse}")
+        # Load the datasets
+        datasets = p_utils.load_population_datasets(mouse, sessions)
+
+        # Pull relevant data into lists
+        ## Parameters
+        imaging_parameters = datasets["Early"].imaging_parameters
+        matched = imaging_parameters["Matched"]
+        ## Lever related data
+        lever_active = [x.lever_active for x in list(datasets.values())]
+        lever_force = [x.lever_force_smooth for x in list(datasets.values())]
+        lever_active_rwd = [x.rewarded_movement_binary for x in list(datasets.values())]
+        ## Cell identifiers
+        cell_positions = [x.cell_positions for x in list(datasets.values())]
+        movement_cells_dFoF = [x.movement_cells for x in list(datasets.values())]
+        silent_cells_dFoF = [x.silent_cells for x in list(datasets.values())]
+        rwd_movement_cells_dFoF = [
+            x.reward_movement_cells for x in list(datasets.values())
+        ]
+        movement_cells_spikes = [
+            x.movement_cells_spikes for x in list(datasets.values())
+        ]
+        silent_cells_spikes = [x.silent_cells_spikes for x in list(datasets.values())]
+        rwd_movement_cells_spikes = [
+            x.reward_movement_spikes for x in list(datasets.values())
+        ]
+        ## Activity data
+        dFoF = [x.processed_dFoF for x in list(datasets.values())]
+        spikes = [x.estimated_spikes for x in list(datasets.values())]
+        smooth_spikes = [x.processed_estimated_spikes for x in list(datasets.values())]
+        activity = [x.activity_trace for x in list(datasets.values())]
+        ## z-scored activity
+        zscore_dFoF = d_utils.z_score(dFoF)
+        zscore_spikes = d_utils.z_score(spikes)
+        zscore_smooth_spikes = d_utils.z_score(smooth_spikes)
+
+        # Get overall activity rates for each cell
+        cell_activity_rate = {}
+        for session, events in zip(sessions, activity):
+            cell_activity_rate[session] = d_utils.calculate_activity_event_rate(events)
+
+        # Get fractions of cell types
+        fraction_MRNs_dFoF = get_fraction_MRNs(sessions, movement_cells_dFoF)
+        fraction_MRNs_spikes = get_fraction_MRNs(sessions, movement_cells_spikes)
+        fraction_rMRNs_dFoF = get_fraction_MRNs(sessions, rwd_movement_cells_dFoF)
+        fraction_rMRNs_spikes = get_fraction_MRNs(sessions, rwd_movement_cells_spikes)
+        fraction_silent_dFoF = get_fraction_MRNs(sessions, silent_cells_dFoF)
+        fraction_silent_spikes = get_fraction_MRNs(sessions, silent_cells_spikes)
+
+        # Get the movement encoding properties of cells
+        (
+            movement_correlation,
+            movement_stereotypy,
+            movement_reliability,
+            movement_specificity,
+        ) = calculate_movement_encoding(
+            mouse,
+            sessions,
+            activity,
+            lever_active,
+            lever_force,
+            threshold=0.5,
+            corr_duration=0.5,
+            sampling_rate=imaging_parameters["Sampling Rate"],
+        )
 
