317e5825e6ef2d1ba955df2172eb4bd88e4b69c3
Jake Wright <wrightwj8@gmail.com>
2023-07-21 12:02:42 -0700

    added rel volume outputs

--- Lab_Analyses/Spine_Analysis_v2/local_coactivity_dataclass.py
+++ Lab_Analyses/Spine_Analysis_v2/local_coactivity_dataclass.py
@@ -60,6 +60,9 @@ class Local_Coactivity_Data:
     avg_nearby_spine_volume: np.ndarray
     shuff_nearby_spine_volume: np.ndarray
     nearby_spine_volume_distribution: np.ndarray
+    local_relative_vol: np.ndarray
+    shuff_relative_vol: np.ndarray
+    relative_vol_distribution: np.ndarray
     local_nn_enlarged: np.ndarray
     shuff_nn_enlarged: np.ndarray
     enlarged_spine_distribution: np.ndarray
