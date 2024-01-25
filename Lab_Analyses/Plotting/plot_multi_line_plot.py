a8f1a062b84c8ed1549bf96afca19849af6158b9
Jake Wright <wrightwj8@gmail.com>
2024-01-25 15:19:52 -0800

    formatting changes

--- Lab_Analyses/Plotting/plot_multi_line_plot.py
+++ Lab_Analyses/Plotting/plot_multi_line_plot.py
@@ -34,44 +34,44 @@ def plot_multi_line_plot(
     save_path=None,
 ):
     """General function to plot a single or multiline plot
-    
-        INPUT PARAMETERS
-            data_dict - dict of 2d np.arrays of the data to be plotted. Each column 
-                        represents a different data point and rows represent different
-                        time points. Keys represent group names
-            
-            x_vals - np.array of the values corresponding to the x axis
-            
-            plot_ind - boolean specifying whether to plot each individual data point
-            
-            figsize - tuple specifying the size of the figure
-            
-            title - str specifying the title of the plot
-            
-            ytitle - str specifying the title of the y axis
-            
-            xtitle - str specifying the title of the x axis
-            
-            ylim -  tuple specifying the limits of the y axis
 
-            line_color - str or list of str specifying the color of the mean lines
+    INPUT PARAMETERS
+        data_dict - dict of 2d np.arrays of the data to be plotted. Each column
+                    represents a different data point and rows represent different
+                    time points. Keys represent group names
 
-            face_color - str or list of str specifying the color of the marker faces
+        x_vals - np.array of the values corresponding to the x axis
 
-            m_size - int specifying the sice of the mean markers
-            
-            axis_width - int or float specifying how thick the axis lines should be
+        plot_ind - boolean specifying whether to plot each individual data point
 
-            minor_ticks - str specifying if minor ticks should be add to the x and/or y 
-                          axes. Takes "both", "x", and "y" as inputs.
+        figsize - tuple specifying the size of the figure
 
-            tick_len - int or float specifying how long the tick marks should be
+        title - str specifying the title of the plot
 
-            ax - axis object you wish the data to be plotted on. Useful for subplotting
+        ytitle - str specifying the title of the y axis
 
-            save - boolean specifying if you wish to save the figure or not
+        xtitle - str specifying the title of the x axis
 
-            save_path - str specifying the path of where to save the figure
+        ylim -  tuple specifying the limits of the y axis
+
+        line_color - str or list of str specifying the color of the mean lines
+
+        face_color - str or list of str specifying the color of the marker faces
+
+        m_size - int specifying the sice of the mean markers
+
+        axis_width - int or float specifying how thick the axis lines should be
+
+        minor_ticks - str specifying if minor ticks should be add to the x and/or y
+                      axes. Takes "both", "x", and "y" as inputs.
+
+        tick_len - int or float specifying how long the tick marks should be
+
+        ax - axis object you wish the data to be plotted on. Useful for subplotting
+
+        save - boolean specifying if you wish to save the figure or not
+
+        save_path - str specifying the path of where to save the figure
     """
     # Check if axis was passed
     if ax is None:
@@ -109,7 +109,12 @@ def plot_multi_line_plot(
         ## Plot mean values
         if m_size is None:
             ax.errorbar(
-                x, mean, yerr=sem, color=line_color[i], linestyle=linestyle, label=key,
+                x,
+                mean,
+                yerr=sem,
+                color=line_color[i],
+                linestyle=linestyle,
+                label=key,
             )
         else:
             ax.errorbar(
