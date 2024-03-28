002e82502a8fa86b0f1531f6217dd3f943e37dce
Jake Wright <wrightwj8@gmail.com>
2024-03-28 16:50:32 -0700

    changed the paths

--- Lab_Analyses/Population_Analysis/population_utilities.py
+++ Lab_Analyses/Population_Analysis/population_utilities.py
@@ -5,18 +5,18 @@ from Lab_Analyses.Utilities.save_load_pickle import load_pickle
 
 def load_population_datasets(mouse_id, sessions):
     """Function to handle loading all of the population datasets for a mouse
-    
-        INPUT PARAMETERS
-            mouse_id - str specifyiing which mouse to load
-            
-            sessions - list of str specifying which sessions to load.
-                        Used for file name searching
-                        
-        OUTPUT PARAMETERS
-            mouse_data - dict containing the data for each day
-            
+
+    INPUT PARAMETERS
+        mouse_id - str specifyiing which mouse to load
+
+        sessions - list of str specifying which sessions to load.
+                    Used for file name searching
+
+    OUTPUT PARAMETERS
+        mouse_data - dict containing the data for each day
+
     """
-    initial_path = r"C:\Users\Jake\Desktop\Analyzed_data\individual"
+    initial_path = r"G:\Analyzed_data\individual"
 
     data_path = os.path.join(initial_path, mouse_id, "population_data")
     fnames = next(os.walk(data_path))[2]
