a5313f2614066daa9c1b6f94ae928260856a59b7
Jake Wright <wrightwj8@gmail.com>
2024-05-02 14:54:52 -0700

    added func to try to estimate event onset for individual movements

--- Lab_Analyses/Utilities/movement_related_activity_v2.py
+++ Lab_Analyses/Utilities/movement_related_activity_v2.py
@@ -22,35 +22,35 @@ def movement_related_activity(
     activity_window=(-2, 4),
 ):
     """Function to analyze activity traces during movements. Only focuses on movements
-        during which that spine was active
-        
-        INPUT PARAMETERS
-            lever_active - np.array of the binarized movement trace
+    during which that spine was active
 
-            activity - 2d np.array of the binarized activity for all ROIs
-            
-            dFoF - 2d np.array of the dFoF traces for all ROIs
-            
-            norm - list of constants to normalize activity by
+    INPUT PARAMETERS
+        lever_active - np.array of the binarized movement trace
 
-            smooth - boolean specifying whether or not to smooth the traces
+        activity - 2d np.array of the binarized activity for all ROIs
 
-            avg_window - float specifying the window you wish to average the peak
-                        over. If None, it will use on the max peak amplitude
-            
-            sampling_rate - int specifying the imaging rate
-            
-            activity_window - tuple specifying the period around movement to analyze
-                              in seconds
-        
-        OUTPUT PARAMETERS
-            movement_traces - list of 2d np.arrays containing the activity during each 
-                            movement (col) for each roi (item in list)
-            
-            movement_amplitudes - np.array of the max amplitudes of the mean traces 
-                                  for each roi
-            
-            relative_onsets - np.array of the activity onset relative to movement onset
+        dFoF - 2d np.array of the dFoF traces for all ROIs
+
+        norm - list of constants to normalize activity by
+
+        smooth - boolean specifying whether or not to smooth the traces
+
+        avg_window - float specifying the window you wish to average the peak
+                    over. If None, it will use on the max peak amplitude
+
+        sampling_rate - int specifying the imaging rate
+
+        activity_window - tuple specifying the period around movement to analyze
+                          in seconds
+
+    OUTPUT PARAMETERS
+        movement_traces - list of 2d np.arrays containing the activity during each
+                        movement (col) for each roi (item in list)
+
+        movement_amplitudes - np.array of the max amplitudes of the mean traces
+                              for each roi
+
+        relative_onsets - np.array of the activity onset relative to movement onset
     """
     center_point = int(np.absolute(activity_window[0]) * sampling_rate)
 
@@ -117,3 +117,63 @@ def movement_related_activity(
 
     return movement_traces, movement_amplitudes, relative_onsets
 
+
+def get_movement_onsets(lever_active, activity, sampling_rate, activity_window):
+    """Function to estimate activity onset for each movement an roi is active
+    during
+
+    INPUT PARAMETERS
+        lever_active - 1d binary np.ndarray of the lever activity
+
+        activity - 2d binary np.ndarray of the roi activity traces. Each column
+                    represents an roi
+
+        sampling_rate - int specifying the imaging rate
+
+        activity_window - tuple specifying the window around movement to analyze
+
+    OUTPUT PARAMETERS
+        movement_onsets - list of 1d arrays containing all onsets for each roi
+
+        movement_onset_jitter - np.array of the average jitter for each roi
+
+    """
+    before_f = int(activity_window[0] * sampling_rate)
+    after_f = int(activity_window[1] * sampling_rate)
+    center_point = np.absolute(before_f)
+
+    # Get movement timestamps
+    timestamps = get_activity_timestamps(lever_active)
+    timestamps = refine_activity_timestamps(
+        timestamps,
+        window=activity_window,
+        max_len=activity.shape[0],
+        sampling_rate=sampling_rate,
+    )
+    timestamps = [x[0] for x in timestamps]
+
+    movement_onsets = []
+    movement_onsets_jitter = []
+
+    for roi in range(activity.shape[1]):
+        curr_activity = activity[:, roi]
+        temp_onsets = []
+        for movement in timestamps:
+            mvmt_activity = curr_activity[movement + before_f : movement + after_f]
+            if not np.nansum(mvmt_activity):
+                movement_onsets.append(None)
+                movement_onsets_jitter.append(np.nan)
+                continue
+            # Get activity boundaries
+            boundaries = np.insert(np.diff(mvmt_activity), 0, 0, axis=0)
+            try:
+                onset = np.nonzero(boundaries == 1)[0][0]
+                rel_onset = (onset - center_point) / sampling_rate
+            except IndexError:
+                rel_onset = np.nan
+            temp_onsets.append(rel_onset)
+        # Store roi values
+        movement_onsets.append(temp_onsets)
+        movement_onsets_jitter.append(np.nanstd(temp_onsets))
+
+    return movement_onsets, movement_onsets_jitter
