d752a4866fb75bae8e35ca8bc12298a55802f619
Jake Wright <wrightwj8@gmail.com>
2024-06-26 13:41:45 -0700

    added peak normalization to trace min

--- Lab_Analyses/Utilities/mean_trace_functions.py
+++ Lab_Analyses/Utilities/mean_trace_functions.py
@@ -2,7 +2,11 @@ import numpy as np
 import scipy.signal as sysignal
 
 from Lab_Analyses.Utilities import data_utilities as d_utils
-from Lab_Analyses.Utilities.activity_timestamps import timestamp_onset_correction
+from Lab_Analyses.Utilities.activity_timestamps import (
+    get_activity_timestamps,
+    refine_activity_timestamps,
+    timestamp_onset_correction,
+)
 
 
 def analyze_event_activity(
@@ -171,7 +175,7 @@ def find_peak_amplitude(
                 trace[trace_peak - avg_win : trace_peak + avg_win]
             )
         else:
-            trace_amplitude = trace[trace_peak]
+            trace_amplitude = trace[trace_peak] - np.min(trace)
 
         trace_amplitudes[i] = trace_amplitude
         trace_amplitudes_idx[i] = trace_peak
@@ -228,3 +232,126 @@ def find_activity_onset(mean_traces, sampling_rate=60):
         activity_onsets[i] = onset
 
     return activity_onsets
+
+
+def max_and_avg_peaks(
+    dFoF,
+    activity,
+    activity_window=(-1, 2),
+    smooth=True,
+    peak_required=True,
+    sampling_rate=60,
+):
+    """Helper function to get max and average amplitudes and
+        traces for individual events for each spine
+
+    INPUT PARAMETERS
+        dFoF - 2d np.ndarray of the dFoF traces with columns
+                corresponding to each ROI
+
+        activity - 2d np.ndarry of the binarized activity with columns
+                    corresponding to each ROI
+
+        activity_window - tuple specifying the window to analyze events
+
+        smooth - boolean specifying whether to smooth the traces for
+                calculating the amplitudes
+
+        peak_required - boolean specifying if the event must have a discernable peak
+                    or not to be included in analysis
+
+        OUTPUT PARAMETERS
+            max_amplitudes - np.array of the the max amplitudes for each ROI
+
+            max_traces - list of 1d arrays of the max event traces for each ROI
+
+            mean_amplitudes - np.ndarray of the mean amplitudes for each ROI
+
+            mean_traces - list of 1d arrays of the averaged trace for each ROI
+
+
+    """
+    DISTANCE = 0.5 * sampling_rate  # constant used for peak detection
+    center_point = int(np.absolute(activity_window[0]) * sampling_rate)
+    # Set up outputs
+    max_amplitudes = []
+    max_traces = []
+    mean_amplitudes = []
+    mean_traces = []
+
+    # Analyze each spine
+    for spine in range(dFoF.shape[1]):
+        curr_dFoF = dFoF[:, spine]
+        curr_activity = activity[:, spine]
+        # Get event onset times
+        timestamps = get_activity_timestamps(curr_activity)
+        timestamps = [x[0] for x in timestamps]
+        timestamps = refine_activity_timestamps(
+            timestamps, activity_window, len(curr_activity), sampling_rate
+        )
+        # Continue if there are not events
+        if len(timestamps) == 0:
+            max_amplitudes.append(np.nan)
+            max_traces.append(None)
+            mean_amplitudes.append(np.nan)
+            mean_traces.append(None)
+            continue
+        # Grab the traces around each event
+        traces, _ = d_utils.get_trace_mean_sem(
+            curr_dFoF.reshape(-1, 1),
+            ["Activity"],
+            timestamps,
+            activity_window,
+            sampling_rate,
+        )
+        traces = traces["Activity"]
+
+        # Temp variables
+        temp_amps = []
+
+        # Analyze each event
+        for event in range(traces.shape[1]):
+            t = traces[:, event]
+            # Smooth trace
+            if smooth:
+                t_smooth = sysignal.savgol_filter(t, 31, 3)
+            else:
+                t_smooth = t
+
+            t_smooth = t_smooth - t_smooth[0]
+
+            # Set up find peak parameters
+            med = np.nanmedian(t_smooth)
+            std = np.nanstd(t_smooth)
+            height = med + std
+            peaks, props = sysignal.find_peaks(
+                t_smooth, height=height, distance=DISTANCE
+            )
+            amps = props["peak_heights"]
+
+            # Get the max amplitude
+            try:
+                max_peak = peaks[np.argmax(amps)]
+            except:
+                if peak_required is False:
+                    max_peak = np.argmax(t_smooth[center_point:])
+                else:
+                    temp_amps.append(np.nan)
+                    continue
+            max_amp = t_smooth[max_peak]
+            # Store value
+            temp_amps.append(max_amp)
+
+        # Find max peak amplitude and traces
+        max_amplitude = np.nanmax(temp_amps)
+        max_trace = traces[:, np.nanargmax(temp_amps)]
+        # Find average amps and traces
+        mean_amplitude = np.nanmean(temp_amps)
+        mean_trace = np.nanmean(traces, axis=1)
+        # Store values
+        max_amplitudes.append(max_amplitude)
+        max_traces.append(max_trace)
+        mean_amplitudes.append(mean_amplitude)
+        mean_traces.append(mean_trace)
+
+    return np.array(max_amplitudes), max_traces, np.array(mean_amplitudes), mean_traces
