de5d237e5a6f9135212127f466b36b1d0a74bc01
Jake Wright <wrightwj8@gmail.com>
2024-06-26 13:46:23 -0700

    added plotting

--- Lab_Analyses/Spine_Analysis_v2/spine_calcium_vs_volume.py
+++ Lab_Analyses/Spine_Analysis_v2/spine_calcium_vs_volume.py
@@ -15,6 +15,7 @@ from Lab_Analyses.Spine_Analysis_v2.structural_plasticity import (
 )
 from Lab_Analyses.Utilities import activity_timestamps as t_stamps
 from Lab_Analyses.Utilities import data_utilities as d_utils
+from Lab_Analyses.Utilities.coactivity_functions import get_conservative_coactive_binary
 from Lab_Analyses.Utilities.mean_trace_functions import analyze_event_activity
 from Lab_Analyses.Utilities.save_load_pickle import load_pickle, save_pickle
 
@@ -22,7 +23,7 @@ from Lab_Analyses.Utilities.save_load_pickle import load_pickle, save_pickle
 def spine_calcium_vs_volume(
     mouse_list, fov_type="apical", plot=False, save=False, save_path=None
 ):
-    """"Function to compare calcium event and volume changes"""
+    """ "Function to compare calcium event and volume changes"""
 
     pre_calcium_amplitudes = []
     post_calcium_amplitudes = []
@@ -33,7 +34,11 @@ def spine_calcium_vs_volume(
     delta_volumes = []
 
     for mouse in mouse_list:
-        datasets = load_spine_datasets(mouse, ["Early", "Middle"], fov_type=fov_type,)
+        datasets = load_spine_datasets(
+            mouse,
+            ["Early", "Middle"],
+            fov_type=fov_type,
+        )
         for FOV, dataset in datasets.items():
             pre_data = dataset["Early"]
             post_data = dataset["Middle"]
@@ -45,6 +50,7 @@ def spine_calcium_vs_volume(
             pix_to_um = pre_data.imaging_parameters["Zoom"] / 2
             pre_volume_um = (np.sqrt(pre_volume) / pix_to_um) ** 2
             pre_flags = pre_data.spine_flags
+            pre_dend_activity = pre_data.dendrite_calcium_activity
             # Pull post-session data
             post_activity = post_data.spine_GluSnFr_activity
             post_calcium = post_data.spine_calcium_processed_dFoF
@@ -52,6 +58,7 @@ def spine_calcium_vs_volume(
             pix_to_um = post_data.imaging_parameters["Zoom"] / 2
             post_volume_um = (np.sqrt(post_volume) / pix_to_um) ** 2
             post_flags = post_data.spine_flags
+            post_dend_activity = post_data.dendrite_calcium_activity
 
             # Calulate volume change
             relative_volume, stable_idxs = calculate_volume_change(
@@ -62,50 +69,80 @@ def spine_calcium_vs_volume(
             )
             # Subselect the stable spine data
             pre_activity = d_utils.subselect_data_by_idxs(pre_activity, stable_idxs)
-            pre_calcium = d_utils.subselect_data_by_idxs(pre_calcium, stable_idxs,)
-            post_activity = d_utils.subselect_data_by_idxs(post_activity, stable_idxs,)
+            pre_calcium = d_utils.subselect_data_by_idxs(
+                pre_calcium,
+                stable_idxs,
+            )
+            pre_dend_activity = d_utils.subselect_data_by_idxs(
+                pre_dend_activity, stable_idxs
+            )
+            post_activity = d_utils.subselect_data_by_idxs(
+                post_activity,
+                stable_idxs,
+            )
             post_calcium = d_utils.subselect_data_by_idxs(post_calcium, stable_idxs)
+            post_dend_activity = d_utils.subselect_data_by_idxs(
+                post_dend_activity, stable_idxs
+            )
 
             # Get activity onsets
             pre_onsets = []
             post_onsets = []
             for i in range(pre_activity.shape[1]):
-                pre_stamps = t_stamps.get_activity_timestamps(pre_activity[:, i])
-                post_stamps = t_stamps.get_activity_timestamps(post_activity[:, i])
+                _, pre_act = get_conservative_coactive_binary(
+                    pre_activity[:, i], pre_dend_activity[:, i]
+                )
+                # pre_act = pre_activity[:, i]
+                pre_stamps = t_stamps.get_activity_timestamps(pre_act)
+                _, post_act = get_conservative_coactive_binary(
+                    post_activity[:, i], post_dend_activity[:, i]
+                )
+                # post_act = post_activity[:, i]
+                post_stamps = t_stamps.get_activity_timestamps(post_act)
                 pre_stamps = t_stamps.refine_activity_timestamps(
                     pre_stamps,
                     window=(-1, 2),
                     max_len=(len(pre_activity[:, i])),
                     sampling_rate=sampling_rate,
                 )
+                pre_stamps = [x[0] for x in pre_stamps]
                 post_stamps = t_stamps.refine_activity_timestamps(
                     post_stamps,
                     window=(-1, 2),
                     max_len=len(post_activity[:, i]),
                     sampling_rate=sampling_rate,
                 )
+                post_stamps = [x[0] for x in post_stamps]
                 pre_onsets.append(pre_stamps)
                 post_onsets.append(post_stamps)
 
             # Get the traces and amplitudes
-            (pre_cal_traces, pre_calcium_amplitude, _,) = analyze_event_activity(
+            (
+                pre_cal_traces,
+                pre_calcium_amplitude,
+                _,
+            ) = analyze_event_activity(
                 pre_calcium,
                 pre_onsets,
                 activity_window=(-1, 2),
                 center_onset=True,
                 smooth=True,
                 avg_window=None,
-                norm_constants=None,
+                norm_constant=None,
                 sampling_rate=sampling_rate,
             )
-            (post_cal_traces, post_calcium_amplitude, _,) = analyze_event_activity(
+            (
+                post_cal_traces,
+                post_calcium_amplitude,
+                _,
+            ) = analyze_event_activity(
                 post_calcium,
                 post_onsets,
                 activity_window=(-1, 2),
                 center_onset=True,
                 smooth=True,
                 avg_window=None,
-                norm_constants=None,
+                norm_constant=None,
                 sampling_rate=sampling_rate,
             )
 
@@ -117,112 +154,110 @@ def spine_calcium_vs_volume(
             pre_calcium_traces.append(pre_cal_traces)
             post_calcium_traces.append(post_cal_traces)
 
-        # Concatenate values
-        pre_calcium_amplitudes = np.concatenate(pre_calcium_amplitudes)
-        post_calcium_amplitudes = np.concatenate(post_calcium_amplitudes)
-        post_volumes = np.concatenate(post_volumes)
-        pre_volumes = np.concatenate(pre_volumes)
-        delta_volumes = np.concatenate(delta_volumes)
-        pre_calcium_traces = [y for x in pre_calcium_traces for y in x]
-        post_calcium_traces = [y for x in post_calcium_traces for y in x]
+    # Concatenate values
+    pre_calcium_amplitudes = np.concatenate(pre_calcium_amplitudes)
+    post_calcium_amplitudes = np.concatenate(post_calcium_amplitudes)
+    post_volumes = np.concatenate(post_volumes)
+    pre_volumes = np.concatenate(pre_volumes)
+    delta_volumes = np.concatenate(delta_volumes)
+    pre_calcium_traces = [y for x in pre_calcium_traces for y in x]
+    post_calcium_traces = [y for x in post_calcium_traces for y in x]
 
-        # caluculate calcium amp change
-        delta_calcium = post_calcium_amplitudes / pre_calcium_amplitudes
+    # caluculate calcium amp change
+    delta_calcium = post_calcium_amplitudes / pre_calcium_amplitudes
 
-        (enlarged, shrunken, stable,) = classify_plasticity(
-            delta_volumes, (0.25, 0.5), norm=False
-        )
+    (
+        enlarged,
+        shrunken,
+        stable,
+    ) = classify_plasticity(delta_volumes, (0.25, 0.5), norm=False)
 
-        plastic_groups = {"sLTP": "enlarged", "sLTD": "shrunken", "Stable": "stable"}
+    plastic_groups = {"sLTP": "enlarged", "sLTD": "shrunken", "Stable": "stable"}
 
-        plastic_delta_calcium_amps = {}
-        plastic_pre_trace_means = {}
-        plastic_pre_trace_sems = {}
-        plastic_post_trace_means = {}
-        plastic_post_trace_sems = {}
+    plastic_delta_calcium_amps = {}
+    plastic_pre_trace_means = {}
+    plastic_pre_trace_sems = {}
+    plastic_post_trace_means = {}
+    plastic_post_trace_sems = {}
 
-        for key, value in plastic_groups.items():
-            spines = eval(value)
-            pre_traces = compress(pre_calcium_traces, spines)
-            pre_means = [
-                np.nanmean(x, axis=1) for x in pre_traces if type(x) == np.ndarray
-            ]
-            pre_means = np.vstack(pre_means)
-            plastic_pre_trace_means[key] = np.nanmean(pre_means, axis=0)
-            plastic_pre_trace_sems[key] = stats.sem(
-                pre_means, axis=0, nan_policy="omit"
-            )
+    for key, value in plastic_groups.items():
+        spines = eval(value)
+        pre_traces = compress(pre_calcium_traces, spines)
+        pre_means = [np.nanmean(x, axis=1) for x in pre_traces if type(x) == np.ndarray]
+        pre_means = np.vstack(pre_means)
+        plastic_pre_trace_means[key] = np.nanmean(pre_means, axis=0)
+        plastic_pre_trace_sems[key] = stats.sem(pre_means, axis=0, nan_policy="omit")
 
-            post_traces = compress(post_calcium_traces, spines)
-            post_means = [
-                np.nanmean(x, axis=1) for x in post_traces if type(x) == np.ndarray
-            ]
-            post_means = np.vstack(post_means)
-            plastic_post_trace_means[key] = np.nanmean(post_means, axis=0)
-            plastic_post_trace_sems[key] = stats.sem(
-                post_means, axis=0, nan_policy="omit"
-            )
-            plastic_delta_calcium_amps[key] = delta_calcium[spines]
+        post_traces = compress(post_calcium_traces, spines)
+        post_means = [
+            np.nanmean(x, axis=1) for x in post_traces if type(x) == np.ndarray
+        ]
+        post_means = np.vstack(post_means)
+        plastic_post_trace_means[key] = np.nanmean(post_means, axis=0)
+        plastic_post_trace_sems[key] = stats.sem(post_means, axis=0, nan_policy="omit")
+        plastic_delta_calcium_amps[key] = delta_calcium[spines]
 
-        if not plot:
-            return
-        # Generate some plots
-        fig, axes = plt.subplot_mosaic(
-            """
-            AB.
-            DEF
-            """,
-            figsize=(5, 5),
-        )
-        fig.suptitle("Volume and Calcium Relationship")
-        plot_scatter_correlation(
-            x_var=delta_volumes,
-            y_var=delta_calcium,
-            CI=95,
-            title=None,
-            xtitle="\u0394 volume",
-            ytitle="\u0394 calcium amplitude",
-            figsize=(5, 5),
-            xlim=None,
-            ylim=None,
-            marker_size=15,
-            face_color="cmap",
-            edge_color="white",
-            line_color="black",
-            s_alpha=1,
-            line_width=1.5,
-            axis_width=1.5,
-            minor_ticks="both",
-            tick_len=3,
-            unity=True,
-            ax=axes["A"],
-            save=False,
-            save_path=None,
-        )
-        plot_box_plot(
-            data_dict=plastic_delta_calcium_amps,
-            figsize=(5, 5),
-            title=None,
-            ytitle="\u0394 calcium amplitude",
-            ylim=(0, None,),
-            b_colors=["mediumslateblue", "tomato", "silver"],
-            b_edgecolors="black",
-            b_err_colors="black",
-            m_color="black",
-            m_width=1.5,
-            b_width=0.5,
-            b_linewidth=1.5,
-            b_alpha=0.9,
-            b_err_alpha=1,
-            whisker_lim=None,
-            whisk_width=1.5,
-            outliers=False,
-            showmeans=True,
-            axis_width=1.5,
-            minor_ticks="y",
-            tick_len=3,
-            ax=axes["B"],
-            save=False,
-            save_path=None,
-        )
-
+    if not plot:
+        return
+    # Generate some plots
+    fig, axes = plt.subplot_mosaic(
+        """
+        AB
+        """,
+        figsize=(5, 4),
+    )
+    fig.suptitle("Volume and Calcium Relationship")
+    plot_scatter_correlation(
+        x_var=delta_volumes,
+        y_var=delta_calcium,
+        CI=95,
+        title=None,
+        xtitle="\u0394 volume",
+        ytitle="\u0394 calcium amplitude",
+        figsize=(5, 5),
+        xlim=(0, 6),
+        ylim=(0, 6),
+        marker_size=15,
+        face_color="cmap",
+        edge_color="white",
+        line_color="black",
+        s_alpha=1,
+        line_width=1.5,
+        axis_width=1.5,
+        minor_ticks="both",
+        tick_len=3,
+        unity=True,
+        ax=axes["A"],
+        save=False,
+        save_path=None,
+    )
+    plot_box_plot(
+        data_dict=plastic_delta_calcium_amps,
+        figsize=(5, 5),
+        title=None,
+        ytitle="\u0394 calcium amplitude",
+        ylim=(
+            0,
+            None,
+        ),
+        b_colors=["mediumslateblue", "tomato", "silver"],
+        b_edgecolors="black",
+        b_err_colors="black",
+        m_color="black",
+        m_width=1.5,
+        b_width=0.5,
+        b_linewidth=1.5,
+        b_alpha=0.9,
+        b_err_alpha=1,
+        whisker_lim=None,
+        whisk_width=1.5,
+        outliers=False,
+        showmeans=True,
+        axis_width=1.5,
+        minor_ticks="y",
+        tick_len=3,
+        ax=axes["B"],
+        save=False,
+        save_path=None,
+    )
+    fig.tight_layout()
