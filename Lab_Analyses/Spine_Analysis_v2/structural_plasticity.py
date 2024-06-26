140036c96aab0e79d242b8635a38ae9c0f1c68f0
Jake Wright <wrightwj8@gmail.com>
2024-06-26 13:43:49 -0700

    added func to calc fract plastic

--- Lab_Analyses/Spine_Analysis_v2/structural_plasticity.py
+++ Lab_Analyses/Spine_Analysis_v2/structural_plasticity.py
@@ -8,26 +8,29 @@ from Lab_Analyses.Spine_Analysis_v2.spine_utilities import (
 
 
 def calculate_volume_change(
-    volume_list, flag_list, norm=False, exclude=None,
+    volume_list,
+    flag_list,
+    norm=False,
+    exclude=None,
 ):
     """Function to calculate relative volume change across all spines
-    
-        INPUT PARAMETERS
-            volume_list - list of np.arrays of spine volumes, with each array corresponding
-                         to a different session
-            
-            flag_list - list of lists containing the spine flags corresponding to the spines
-                        in the spines in the volume list
-            
-            norm - boolean term specifying whether to calculate normalized volume changes
-            
-            exclude - str specifying a type of spine to exclude from the analysis
-            
-        OUTPUT PARAMETERS
-            relative_volumes - list of arrays containing the relative volume change for each
-                                session
-            
-            stable_idxs - np.array of the indexes of the stable spines
+
+    INPUT PARAMETERS
+        volume_list - list of np.arrays of spine volumes, with each array corresponding
+                     to a different session
+
+        flag_list - list of lists containing the spine flags corresponding to the spines
+                    in the spines in the volume list
+
+        norm - boolean term specifying whether to calculate normalized volume changes
+
+        exclude - str specifying a type of spine to exclude from the analysis
+
+    OUTPUT PARAMETERS
+        relative_volumes - list of arrays containing the relative volume change for each
+                            session
+
+        stable_idxs - np.array of the indexes of the stable spines
     """
     # Get the stable spine idxs
     stable_spines = find_stable_spines_across_days(flag_list)
@@ -59,22 +62,22 @@ def calculate_volume_change(
 
 def classify_plasticity(relative_volumes, threshold=0.3, norm=False):
     """Function to classify if spines have undergo enlargement or shrinkage
-        based on a given threshold
-        
-        INPUT PARAMETERS
-            relative_volumes - list of relative volume changes for each spine
-            
-            threshold - tuple or float specifying what the cutoff for enlargement
-                        and shrinkage are
+    based on a given threshold
 
-            norm - boolean specifying if the relative volumes are normalized or not
-        
-        OUTPUT PARAMETERS
-            enlarged_spines - boolean list of spines classified as enlarged
+    INPUT PARAMETERS
+        relative_volumes - list of relative volume changes for each spine
 
-            shrunken_spines - boolean list of spines classified as shrunkent
+        threshold - tuple or float specifying what the cutoff for enlargement
+                    and shrinkage are
 
-            stable_spiens - boolean list of spines classified as stable
+        norm - boolean specifying if the relative volumes are normalized or not
+
+    OUTPUT PARAMETERS
+        enlarged_spines - boolean list of spines classified as enlarged
+
+        shrunken_spines - boolean list of spines classified as shrunkent
+
+        stable_spiens - boolean list of spines classified as stable
     """
     # Initialize the outputs
     enlarged_spines = [False for x in relative_volumes]
@@ -113,28 +116,31 @@ def classify_plasticity(relative_volumes, threshold=0.3, norm=False):
 def calculate_spine_dynamics(
     spine_flag_list, spine_positions_list, spine_groupings_list
 ):
-    """Function to calculate the spine density and the fraction of new and 
-        eliminated spines along a given dendrite
-        
-        INPUT PARAMETERS
-            spine_flag_list - list lists containing the spine flags for each session
-            
-            spine_position_list - list of  np.array of the spine positions for each session
-            
-            spine_groupinglist - list of list of the spine groupings along the different
-                             dendrites for each session
-        
-        OUTPUT PARAMETERS
-            spine_density - list of np.array of the density of spines along the dendrite
-            
-            fraction_new_spines - list np.array of the fraction of new spines
-            
-            fraction_eliminated_spines - list np.array of the fraction of eliminated spines
+    """Function to calculate the spine density and the fraction of new and
+    eliminated spines along a given dendrite
+
+    INPUT PARAMETERS
+        spine_flag_list - list lists containing the spine flags for each session
+
+        spine_position_list - list of  np.array of the spine positions for each session
+
+        spine_groupinglist - list of list of the spine groupings along the different
+                         dendrites for each session
+
+    OUTPUT PARAMETERS
+        spine_density - list of np.array of the density of spines along the dendrite
+
+        fraction_new_spines - list np.array of the fraction of new spines
+
+        fraction_eliminated_spines - list np.array of the fraction of eliminated spines
     """
     # Set up the outputs
     spine_density = []
     fraction_new_spines = []
     fraction_eliminated_spines = []
+
+    base_flags = spine_flag_list[0]
+    base_present = find_present_spines(base_flags)
 
     # Iterate through each session
     for i, (flags, positions, groupings) in enumerate(
@@ -167,6 +173,7 @@ def calculate_spine_dynamics(
         # Iterate through each dendrite grouping
         for j, spines in enumerate(groupings):
             # Get the current flags
+            base_num = np.sum(base_present[spines])
             curr_flags = [x for i, x in enumerate(temp_flags) if i in spines]
             # Calculate the spine density
             present_spines = find_present_spines(curr_flags)
@@ -175,11 +182,14 @@ def calculate_spine_dynamics(
 
             # Determine the fraction of new spines
             new_spines = find_spine_classes(curr_flags, "New Spine")
-            curr_frac_new[j] = np.sum(new_spines) / np.sum(present_spines)
+            # curr_frac_new[j] = np.sum(new_spines) / np.sum(present_spines)
+            curr_frac_new[j] = np.sum(new_spines) / base_num
+            # curr_frac_new[j] = np.sum(new_spines) / length
 
             # Determine the fraction of eliminated spines
             elim_spines = find_spine_classes(curr_flags, "Eliminated Spine")
-            curr_frac_elim[j] = np.sum(elim_spines) / np.sum(present_spines)
+            curr_frac_elim[j] = np.sum(elim_spines) / base_num
+            # curr_frac_elim[j] = np.sum(elim_spines) / length
 
         # Store the values
         spine_density.append(curr_density)
@@ -188,3 +198,35 @@ def calculate_spine_dynamics(
 
     return spine_density, fraction_new_spines, fraction_eliminated_spines
 
+
+def calculate_fraction_plastic(
+    spine_vol_list, spine_flag_list, threshold=(0.25, 0.5), exclude="Shaft Spine"
+):
+    """Function to calculate the fraction of spines undergoing LTP and LTD.
+    Designed for comparison between only two sessions"""
+
+    # Get base number of present spines
+    base_present = find_present_spines(spine_flag_list[0])
+    ## Remove shaft spines
+    shaft_spines = find_spine_classes(spine_flag_list[0], spine_class="Shaft Spine")
+    shaft_spines = [not x for x in shaft_spines]
+    base_present = base_present * shaft_spines
+    base_num = np.sum(base_present)
+
+    # Find volumes
+    delta_volume, _ = calculate_volume_change(
+        spine_vol_list, spine_flag_list, norm=False, exclude=exclude
+    )
+    delta_volume = delta_volume[-1]
+    # Classify plaslticity
+    enlarged_spines, shrunken_spines, stable_spines = classify_plasticity(
+        delta_volume, threshold=threshold, norm=False
+    )
+
+    # Calculate Fractions
+
+    frac_LTP = np.sum(enlarged_spines) / base_num
+    frac_LTD = np.sum(shrunken_spines) / base_num
+    frac_stable = np.sum(stable_spines) / base_num
+
+    return frac_LTP, frac_LTD, frac_stable
